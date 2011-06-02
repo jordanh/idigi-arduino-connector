1a7409026c832b4a7057334bbc8ee1c7d459e8ec
Ming Liu-Chan <mlchan@digi.com>
2011-06-02 18:10:20 -0400

    Removed device_id field from idigi_data_t structure. Added function prototypes

--- public/sample/linux/idigi_data.h
+++ public/sample/linux/idigi_data.h
@@ -27,7 +27,7 @@
 
 #include <netinet/in.h>
 #include <stdio.h>
-#include "idk_api.h"
+#include "iik_api.h"
 
 #ifdef __cplusplus
 extern "C"
@@ -42,7 +42,6 @@ extern "C"
 #define NETWORK_TIMEOUT_SET   0x01 << 2
 
 typedef struct {
-    uint8_t         device_id[DEVICE_ID_LENGTH];
     uint8_t         vendor_id[VENDOR_ID_LENGTH];
     uint16_t        tx_keepalive;
     uint16_t        rx_keepalive;
@@ -52,17 +51,36 @@ typedef struct {
     char            * password;
     uint8_t         * phone_number;
     uint32_t        link_speed;
-    idk_connection_type_t connection_type;
+    iik_connection_type_t connection_type;
     uint8_t         mac_addr[MAC_ADDR_LENGTH];
 
     uint32_t        ip_addr; /* only IPv4 */
 
-    idk_handle_t             idk_handle;
-    idk_network_handle_t     socket_fd;
+    iik_handle_t             iik_handle;
+    iik_network_handle_t     socket_fd;
     uint8_t                  select_data;
 } idigi_data_t;
 
 extern idigi_data_t iDigiSetting;
+extern time_t  deviceSystemUpStartTime;
+
+iik_callback_status_t idigi_firmware_callback(iik_firmware_request_t request,
+                                              void const * request_data, size_t request_length,
+                                              void * response_data, size_t * response_length);
+
+iik_callback_status_t idigi_config_callback(iik_config_request_t request,
+                                            void const * request_data, size_t request_length,
+                                            void * response_data, size_t * response_length);
+
+iik_callback_status_t idigi_network_callback(iik_network_request_t request,
+                                            void const * request_data, size_t request_length,
+                                            void * response_data, size_t * response_length);
+uint8_t network_select(iik_network_handle_t fd, uint8_t select_set, unsigned wait_time);
+
+iik_callback_status_t idigi_os_callback(iik_os_request_t request,
+                                        void const * request_data, size_t request_length,
+                                        void * response_data, size_t * response_length);
+
 
 #ifdef __cplusplus
 }
