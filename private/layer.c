563458e11d4b15160fa8de755da90d3821d4cbe5
Ming Liu-Chan <mlchan@digi.com>
2011-08-22 16:27:20 -0400

    Removed RCI response

--- private/layer.c
+++ private/layer.c
@@ -952,43 +952,6 @@ enum {
 done:
     return status;
 }
-#if 1
-/* TODO: Remove  */
-static idigi_callback_status_t send_rci_response(idigi_data_t * const idigi_ptr)
-{
-#define RCI_COMMAND_REPLY_START_OPCODE      0x04
-#define RCI_NO_COMPRESSION                 0x00
-
-    idigi_callback_status_t status;
-    uint8_t * packet;
-    uint8_t * ptr;
-    uint8_t * start_ptr;
-    static char const no_query_state_response[] = "<rci_replay version=\"1.1\"> <query_state/> </rci_reply>";
-    uint32_t size = sizeof no_query_state_response -1;
-
-    packet = get_packet_buffer(idigi_ptr, E_MSG_FAC_RCI_NUM, &ptr);
-    if (packet == NULL)
-    {
-        status = idigi_callback_busy;
-        goto done;
-    }
-    start_ptr = ptr;
-
-    *ptr++ = RCI_COMMAND_REPLY_START_OPCODE;
-    *ptr++ = RCI_NO_COMPRESSION;
-
-    StoreBE32(ptr, size);
-    ptr += sizeof size;
-
-    memcpy(ptr, no_query_state_response, size);
-    ptr += size;
-
-    status = initiate_send_facility_packet(idigi_ptr, packet, ptr - start_ptr, E_MSG_FAC_RCI_NUM, release_packet_buffer, NULL);
-
-done:
-    return status;
-}
-#endif
 
 static idigi_callback_status_t facility_layer(idigi_data_t * idigi_ptr)
 {
@@ -1075,18 +1038,6 @@ enum {
              */
             facility = message_load_be16(edp_protocol, facility);
             idigi_ptr->layer_state = facility_receive_message;
-#if 1
-            /* TODO: Remove this RCI response */
-            if (facility == E_MSG_FAC_RCI_NUM)
-            {
-                status = send_rci_response(idigi_ptr);
-                if (status == idigi_callback_busy)
-                {
-                    idigi_ptr->layer_state = facility_process_message;
-                    done_packet = false;
-                }
-            }
-#endif
             for (fac_ptr = idigi_ptr->facility_list; fac_ptr != NULL; fac_ptr = fac_ptr->next)
             {
                 if (fac_ptr->facility_num  == facility)
