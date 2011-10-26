664bad4aed95e530d8d7f86f3c6e4678950287a9
Ming Liu-Chan <mlchan@digi.com>
2011-10-26 15:09:58 -0400

    Added chk_config.h & options.h

---
+++ private/options.h
@@ -0,0 +1,44 @@
+/*
+ *  Copyright (c) 1996-2011 Digi International Inc., All Rights Reserved
+ *
+ *  This software contains proprietary and confidential information of Digi
+ *  International Inc.  By accepting transfer of this copy, Recipient agrees
+ *  to retain this software in confidence, to prevent disclosure to others,
+ *  and to make no use of this software other than that for which it was
+ *  delivered.  This is an unpublished copyrighted work of Digi International
+ *  Inc.  Except as permitted by federal law, 17 USC 117, copying is strictly
+ *  prohibited.
+ *
+ *  Restricted Rights Legend
+ *
+ *  Use, duplication, or disclosure by the Government is subject to
+ *  restrictions set forth in sub-paragraph (c)(1)(ii) of The Rights in
+ *  Technical Data and Computer Software clause at DFARS 252.227-7031 or
+ *  subparagraphs (c)(1) and (2) of the Commercial Computer Software -
+ *  Restricted Rights at 48 CFR 52.227-19, as applicable.
+ *
+ *  Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
+ *
+ * =======================================================================
+ *
+ */
+#ifndef OPTIONS_H_
+#define OPTIONS_H_
+
+#if defined(_LITTLE_ENDIAN)
+#define IDIGI_LITTLE_ENDIAN
+#endif
+
+#if defined(_FIRMWARE_FACILITY)
+#define IDIGI_FIRMWARE_SERVICE
+#endif
+
+#if defined(DEBUG)
+#define IDIGI_DEBUG
+#endif
+
+#if defined(IDIGI_COMPRESSION_BUILTIN)
+#define IDIGI_COMPRESSION
+#endif
+
+#endif /* OPTIONS_H_ */
