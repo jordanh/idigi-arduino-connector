8770d1e54c929350be58d0f84e40f5c65ba5bc59
Vidya Shankar <vshankar@digi.com>
2011-12-08 12:01:27 -0500

    Added method in config.py, so that it can be exported

--- scripts/config.py
+++ scripts/config.py
@@ -3,53 +3,41 @@ import os
 import fileinput, string, sys
 from optparse import OptionParser
 
-parser = OptionParser(usage = "usage: %prog header_file ini_file cnfg_file")
-(options, args) = parser.parse_args()
-
-if len(args) != 3:
-  print "Usage: python config.py <path>/idigi_config.h <path>/config.ini <path>/config.c"
-  raise SyntaxError
-
-header_file = args[0]
-ini_file    = args[1]
-cnfg_file   = args[2]
-
-config = ConfigParser.SafeConfigParser()
-config.read(ini_file)
 new_lines = []
 
-def update_field(line, field, intflag):
+def update_field(config, line, field, isString):
   elements = line.split()
   if elements[0] != '/*':
     new_lines.append(line)
     return
 
   modified_line = elements[1] + '    ' + elements[2] + '    '
-  if intflag:
-    modified_line += config.get("device", field) + '\n'
-  else:
+  if isString:
     modified_line += '"' + config.get("device", field) + '"' + '\n'  
+  else:
+    modified_line += config.get("device", field) + '\n'
   new_lines.append(modified_line)
 
-def update_idigi_config_h():
+def update_idigi_config_h(header_file, config):
 
   infile = open(header_file, "r")
+  new_lines[:] = []
 
   for line in infile:
     if 'IDIGI_DEVICE_TYPE' in line:
-      update_field(line, "device_type", False)
+      update_field(config, line, "device_type", True)
     elif 'IDIGI_CLOUD_URL' in line:
-      update_field(line, "server_url", False)
+      update_field(config, line, "server_url", True)
     elif 'IDIGI_TX_KEEPALIVE_IN_SECONDS' in line:
-      update_field(line, "tx_keepalive", True)
+      update_field(config, line, "tx_keepalive", False)
     elif 'IDIGI_RX_KEEPALIVE_IN_SECONDS' in line:
-      update_field(line, "rx_keepalive", True)
+      update_field(config, line, "rx_keepalive", False)
     elif 'IDIGI_WAIT_COUNT' in line:
-      update_field(line, "wait_count", True)
+      update_field(config, line, "wait_count", False)
     elif 'IDIGI_VENDOR_ID' in line:
-      update_field(line, "vendor_id", True)
+      update_field(config, line, "vendor_id", False)
     elif 'IDIGI_MSG_MAX_TRANSACTION' in line:
-      update_field(line, "max_transaction", True)
+      update_field(config, line, "max_transaction", False)
     else:
       new_lines.append(line)
 
@@ -60,7 +48,7 @@ def update_idigi_config_h():
     outfile.write(line)
   outfile.close()
 
-def update_config_c():
+def update_config_c(cnfg_file, config):
   old_mac = '{0x00, 0x00, 0x00, 0x00, 0x00, 0x00}'
 
   infile = open(cnfg_file, "r")
@@ -83,7 +71,19 @@ def update_config_c():
   outfile.write(text.replace(old_mac, new_mac))
   outfile.close()
 
+def update_config_files(header_file, ini_file, cnfg_file):
+  config = ConfigParser.SafeConfigParser()
+  config.read(ini_file)
+  update_idigi_config_h(header_file, config)
+  update_config_c(cnfg_file, config)
+
 if __name__ == '__main__':
-  update_idigi_config_h()
-  update_config_c()
+  parser = OptionParser(usage = "usage: %prog header_file ini_file cnfg_file")
+  (options, args) = parser.parse_args()
+
+  if len(args) != 3:
+    print "Usage: python config.py <path>/idigi_config.h <path>/config.ini <path>/config.c"
+    raise SyntaxError
+
+  update_config_files(args[0], args[1], args[2])
 
