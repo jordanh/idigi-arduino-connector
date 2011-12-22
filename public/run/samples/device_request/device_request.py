111cb450156ebcf6a539de7063eb3cae0b75a67f
Ming Liu-Chan <mlchan@digi.com>
2011-12-22 16:00:54 -0500

    Updated username, password, & device_id as input argument

--- public/run/samples/device_request/device_request.py
+++ public/run/samples/device_request/device_request.py
@@ -19,56 +19,69 @@
 #
 # ***************************************************************************
 # device_request.py
-# Send device request 
+# Send SCI device_request to server. 
 # -------------------------------------------------
-# The following lines require manual changes
-username = "YourUsername" # enter your username
-password = "YourPassword" # enter your password
-device_id = "Target Device Id" # enter device id of target
+# Usage: device_request.py <username> <password> <device_id>
 # -------------------------------------------------
 import httplib
 import base64
+import sys
+
+def Usage():
+    print 'Usage: device_request.py <username> <password> <device_id>\n'
    
-# create HTTP basic authentication string, this consists of
-# "username:password" base64 encoded
-auth = base64.encodestring("%s:%s"%(username,password))[:-1]
+def PostMessage(username, password, device_id):
+    # create HTTP basic authentication string, this consists of
+    # "username:password" base64 encoded
+    auth = base64.encodestring("%s:%s"%(username,password))[:-1]
+    
+    # device request message to send to server
+    message = """<sci_request version="1.0">
+        <data_service>
+            <targets>
+                <device id="%s"/>
+            </targets>
+            <requests>
+            <device_request target_name="myTarget">My device request data</device_request>
+            </requests>
+        </data_service>
+    </sci_request>
+    """%(device_id)
+    
+    # to what URL to send the request with a given HTTP method
+    webservice = httplib.HTTP("developer.idigi.com",80)
+    webservice.putrequest("POST", "/ws/sci")
+    
+    # add the authorization string into the HTTP header
+    webservice.putheader("Authorization", "Basic %s"%auth)
+    webservice.putheader("Content-type", "text/xml; charset=\"UTF-8\"")
+    webservice.putheader("Content-length", "%d" % len(message))
+    webservice.endheaders()
+    webservice.send(message)
+    
+    # get the response
+    statuscode, statusmessage, header = webservice.getreply()
+    response_body = webservice.getfile().read()
+    
+    # print the output to standard out
+    if statuscode == 200:
+        print '\nResponse:'
+        print response_body
+    else:
+        print '\nError: %d %s' %(statuscode, statusmessage)
+    
+    webservice.close()
 
-# device request message to send to server
-message = """<sci_request version="1.0">
-    <data_service>
-        <targets>
-            <device id="%s"/>
-        </targets>
-        <requests>
-        <device_request target_name="myTarget">My device request data</device_request>
-        </requests>
-    </data_service>
-</sci_request>
-"""%(device_id)
 
-# to what URL to send the request with a given HTTP method
-webservice = httplib.HTTP("developer.idigi.com",80)
-webservice.putrequest("POST", "/ws/sci")
-
-# add the authorization string into the HTTP header
-webservice.putheader("Authorization", "Basic %s"%auth)
-webservice.putheader("Content-type", "text/xml; charset=\"UTF-8\"")
-webservice.putheader("Content-length", "%d" % len(message))
-webservice.endheaders()
-webservice.send(message)
-
-# get the response
-statuscode, statusmessage, header = webservice.getreply()
-response_body = webservice.getfile().read()
-
-# print the output to standard out
-if statuscode == 200:
-    print '\nResponse:'
-    print response_body
-else:
-    print '\nError: %d %s' %(statuscode, statusmessage)
-
-webservice.close()
+def main(argv):
+    #process arguments
+    count = len(argv);
+    if count != 3:
+        Usage()
+    else:
+        PostMessage(argv[0], argv[1], argv[2])
 
 
-
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
+    
