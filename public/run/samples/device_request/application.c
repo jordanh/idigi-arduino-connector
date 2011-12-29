56a419adbb428f5bce0f54b97b7cd5252a1aeb8e
Ming Liu-Chan <mlchan@digi.com>
2011-12-29 14:55:55 -0500

    Replaced idigi_ with app_ prefix

--- public/run/samples/device_request/application.c
+++ public/run/samples/device_request/application.c
@@ -25,7 +25,7 @@
 #include "idigi_api.h"
 #include "platform.h"
 
-extern idigi_callback_status_t idigi_data_service_callback(idigi_data_service_request_t const request,
+extern idigi_callback_status_t app_data_service_handler(idigi_data_service_request_t const request,
                                                   void const * request_data, size_t const request_length,
                                                   void * response_data, size_t * const response_length);
 
@@ -38,16 +38,16 @@ idigi_callback_status_t idigi_callback(idigi_class_t const class_id, idigi_reque
     switch (class_id)
     {
     case idigi_class_config:
-        status = idigi_config_callback(request_id.config_request, request_data, request_length, response_data, response_length);
+        status = app_config_handler(request_id.config_request, request_data, request_length, response_data, response_length);
         break;
     case idigi_class_operating_system:
-        status = idigi_os_callback(request_id.os_request, request_data, request_length, response_data, response_length);
+        status = app_os_handler(request_id.os_request, request_data, request_length, response_data, response_length);
         break;
     case idigi_class_network:
-        status = idigi_network_callback(request_id.network_request, request_data, request_length, response_data, response_length);
+        status = app_network_handler(request_id.network_request, request_data, request_length, response_data, response_length);
         break;
     case idigi_class_data_service:
-        status = idigi_data_service_callback(request_id.data_service_request, request_data, request_length, response_data, response_length);
+        status = app_data_service_handler(request_id.data_service_request, request_data, request_length, response_data, response_length);
         break;
     default:
         /* not supported */
