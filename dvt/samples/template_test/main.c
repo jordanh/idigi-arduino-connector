1959828c04391d9390d6ea5d71bc7fb5167aac43
Ming Liu-Chan <mlchan@digi.com>
2012-01-03 16:02:03 -0500

    initial for template platform build test

---
+++ dvt/samples/template_test/main.c
@@ -0,0 +1,95 @@
+/*
+ *  Copyright (c) 1996-2011 Digi International Inc., All Rights Reserved
+ *
+ *  This software contains proprietary and confidential information of Digi
+ *  International Inc.  By accepting transfer of this copy, Recipient agrees+
+ *  to retain this software in confidence, to prevent disclosure to others,
+ *  and to make no use of this software other than that for which it was
+ *  delivered.  This is an unpublished copyrighted work of Digi International
+ *  Inc.  Except as permitted by federal law, 17 USC 117, copying is strictly
+ *  prohibited.
+ *
+ *  Restricted Rights Legend
+ *
+ *  Use, duplication, or disclosure by the Government is subject to
+ *  restrictions set forth in sub-paragraph (c)(1)(ii) of The Rights in
+ *  Technical Data and Computer Software clause at DFARS 252.227-7031 or
+ *  subparagraphs (c)(1) and (2) of the Commercial Computer Software -
+ *  Restricted Rights at 48 CFR 52.227-19, as applicable.
+ *
+ *  Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
+ *
+ * =======================================================================
+ *
+ */
+
+//#include <unistd.h>
+#include <stdio.h>
+#include "idigi_api.h"
+
+#define APP_DEBUG  printf
+
+extern idigi_callback_status_t idigi_callback(idigi_class_t const class_id, idigi_request_t const request_id,
+                                    void * const request_data, size_t const request_length,
+                                    void * response_data, size_t * const response_length);
+
+idigi_callback_status_t app_os_handler(idigi_os_request_t const request,
+                           void * const request_data, size_t const request_length,
+                           void * response_data, size_t * const response_length);
+
+
+idigi_callback_status_t app_network_handler(idigi_network_request_t const request,
+                         void * const request_data, size_t const request_length,
+                         void * response_data, size_t * const response_length);
+
+idigi_callback_status_t app_config_handler(idigi_config_request_t const request,
+                                              void * const request_data,
+                                              size_t const request_length,
+                                              void * response_data,
+                                              size_t * const response_length);
+
+idigi_callback_status_t idigi_callback(idigi_class_t const class_id, idigi_request_t const request_id,
+                                    void * const request_data, size_t const request_length,
+                                    void * response_data, size_t * const response_length)
+{
+    idigi_callback_status_t   status = idigi_callback_continue;
+
+
+    switch (class_id)
+    {
+    case idigi_class_config:
+        status = app_config_handler(request_id.config_request, request_data, request_length, response_data, response_length);
+        break;
+
+    case idigi_class_operating_system:
+        status = app_os_handler(request_id.os_request, request_data, request_length, response_data, response_length);
+        break;
+
+    case idigi_class_network:
+        status = app_network_handler(request_id.network_request, request_data, request_length, response_data, response_length);
+        break;
+
+    default:
+        /* not supported */
+        break;
+    }
+
+    return status;
+}
+
+int main (void)
+{
+    idigi_handle_t idigi_handle;
+
+    APP_DEBUG("Start iDigi\n");
+    idigi_handle = idigi_init((idigi_callback_t) idigi_callback);
+    if (idigi_handle != NULL)
+    {
+        APP_DEBUG("This is only testing the template platform\n");
+    }
+    else
+    {
+        APP_DEBUG("unable to initialize iDigi\n");
+    }
+    return 0;
+}
