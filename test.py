324fe3fd20afbf9464e2cd8b8510dd5c23802fa3
Ming Liu-Chan <mlchan@digi.com>
2012-01-10 13:42:24 -0500

    Added stderr.txt for standard error from idigi. updated run_test() to display error in stderr.txt and delete stderr.txt. Added admin_test test script for redirect and reboot. Updated run_test() with debug_on parameter since ASSERT will happen for redirect test if debug_on is true

--- test.py
+++ test.py
@@ -28,6 +28,9 @@ import getopt
 import signal
 import imp
 
+import os, time
+from stat import * # ST_SIZE etc
+
 TEMPLATE_TEST_DIR = './dvt/samples/template_test'
 TEMPLATE_SCRIPT_DIR = './dvt/cases/dvt_tests/'
 TEMPLATE_PLATFORM_DIR = './public/run/platforms/template/'
@@ -47,6 +50,8 @@ TEST_LIST = 2
 
 test_case = 0
 
+STDERR_FILE = 'stderr.txt'
+
 #
 # Modify this table when adding a new test.
 # 
@@ -58,11 +63,13 @@ test_case = 0
 # 3. List of python test scripts to run
 #
 #              Directory.                            Script directory                Test Scripts
-test_table = [[BASE_SAMPLE_DIR+'connect_to_idigi',  BASE_SCRIPT_DIR+'sample_tests', ['test_discovery.py']],
+test_table = [
+              [BASE_SAMPLE_DIR+'connect_to_idigi',  BASE_SCRIPT_DIR+'sample_tests', ['test_discovery.py']],
               [BASE_SAMPLE_DIR+'firmware_download', BASE_SCRIPT_DIR+'sample_tests', ['test_firmware.py']],
               [BASE_SAMPLE_DIR+'send_data',         BASE_SCRIPT_DIR+'sample_tests', ['test_send_data.py']],
               [BASE_SAMPLE_DIR+'device_request',    BASE_SCRIPT_DIR+'sample_tests', ['test_device_request.py']],
-              [BASE_DVT_SRC+'full_test',            BASE_SCRIPT_DIR+'dvt_tests',    ['test_firmware_errors.py', 'test_device_request.py', 'test_data_service.py']]
+              [BASE_DVT_SRC+'full_test',            BASE_SCRIPT_DIR+'dvt_tests',    ['test_firmware_errors.py', 'test_device_request.py', 'test_data_service.py']],
+              [BASE_DVT_SRC+'data_service',         BASE_SCRIPT_DIR+'admin_tests',  ['test_reboot.py', 'test_redirect.py']]
 ]
 
 def build_test(dir):
@@ -73,9 +80,8 @@ def build_test(dir):
         print "+++FAIL: Build failed dir=[%s]" % dir
         exit(0)
 
-def run_tests():
-    global test_case
-
+def run_tests(debug_on):
+    i = 0
     for test in test_table:
         src_dir   = test[SRC_DIR]
         test_dir  = test[TEST_DIR]
@@ -86,7 +92,7 @@ def run_tests():
         build_test(src_dir)
 
         print '>>>Starting idigi'
-        rc = os.system('cd %s;./idigi &' % (src_dir))
+        rc = os.system('cd %s;./idigi 2>%s &' % (src_dir, STDERR_FILE))
         if rc != 0:
             print "+++FAIL: Could not start idigi dir=[%s]" % src_dir
             exit(0)
@@ -101,9 +107,12 @@ def run_tests():
         time.sleep(5) # Give the program time to start
 
         for test_script in test_list:
-            print '>>>Executing [%s]' % test_script
-            rc = os.system('export PYTHONPATH=../;cd %s; nosetests --with-xunit --xunit-file=nosestest%1d.xml %s' % (test_dir, test_case, test_script))
-            test_case += 1
+            if debug_on and test_script == 'test_redirect.py':
+                print '>>>Skip [%s] since debug is on' % test_script
+            else:
+                print '>>>Executing [%s]' % test_script
+                rc = os.system('export PYTHONPATH=../;cd %s; nosetests --with-xunit --xunit-file=nosestest%1d.xml %s' % (test_dir, i, test_script))
+                test_case += 1
 
         print '>>>pid [%s]' % pid
         if pid != '':
@@ -111,7 +120,32 @@ def run_tests():
             try:
                 os.kill(p, signal.SIGKILL)
             except OSError as ex:
-                print"OSError as ex"
+                idigi_pid = commands.getoutput('pidof -s idigi')
+                if idigi_pid == '':
+                    print "idigi process not exist"
+                elif p != int(idigi_pid):
+                    print "multiple idigi processes pid [%s, %s]" % (pid, idigi_pid)
+                else: 
+                    print"OSError as ex"
+
+        # open standard error file to see any Error message
+        errorfile = "%s/%s" % (src_dir, STDERR_FILE)
+        try:
+            st = os.stat(errorfile)
+        except IOError:
+            print "failed to get information from %s" % errorfile
+        else:
+            if st[ST_SIZE] != 0:
+                previous_line = ""
+                infile = open(errorfile, "r")
+                for current_line in infile:
+                    if 'Error:' in previous_line:
+                        print "Error: %s" % current_line
+                        rc = -1
+                    previous_line = current_line
+                infile.close()
+            os.system('rm %s' % errorfile);
+
         os.system('cd ../../../../../')
 
         if rc != 0:
@@ -126,21 +160,24 @@ def main():
     f, filename, description = imp.find_module('config', ['./dvt/scripts'])
     config = imp.load_module('config', f, filename, description)
 
+    debug_on = True
     setup_platform(config, SAMPLE_SCRIPT_DIR, SAMPLE_PLATFORM_DIR)
-    run_tests()
+    run_tests(debug_on)
 
+    debug_on = False
     config.replace_string('./public/include/idigi_config.h', 'IDIGI_DEBUG', 'IDIGI_NODEBUG')
-    run_tests()
+    run_tests(debug_on)
 
     config.replace_string('./public/include/idigi_config.h', 'IDIGI_COMPRESSION', 'IDIGI_NOCOMPRESSION')
-    run_tests()
+    run_tests(debug_on)
 
+    debug_on = True
     config.replace_string('./public/include/idigi_config.h', 'IDIGI_NODEBUG', 'IDIGI_DEBUG')
-    run_tests()
+    run_tests(debug_on)
 
     config.replace_string('./public/include/idigi_config.h', 'IDIGI_NOCOMPRESSION', 'IDIGI_COMPRESSION')
     config.update_config_header('./public/include/idigi_config.h', SAMPLE_SCRIPT_DIR+'config.ini')
-    run_tests()
+    run_tests(debug_on)
 
     setup_platform(config, TEMPLATE_SCRIPT_DIR, TEMPLATE_PLATFORM_DIR)
     build_test(TEMPLATE_TEST_DIR)
@@ -155,6 +192,8 @@ def main():
     build_test(TEMPLATE_TEST_DIR)
 
     config.replace_string('./public/include/idigi_config.h', 'IDIGI_NO_DATA_SERVICE', 'IDIGI_DATA_SERVICE')
+
+    
 
 if __name__ == '__main__':
     main()
