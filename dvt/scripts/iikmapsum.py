49b9b331135d522124d8e0c1a83422485b43e096
Vidya Shankar <vshankar@digi.com>
2012-01-12 17:47:38 -0500

    Added memory usage text file for Andy to process and display info on hudson

--- dvt/scripts/iikmapsum.py
+++ dvt/scripts/iikmapsum.py
@@ -8,7 +8,13 @@ rodata_size_in_bytes = 380
 data_size_in_bytes   = 0
 bss_size_in_bytes    = 0
 
-isReleaseBuild = False
+both_enabled = 0
+debug_disabled = 1
+compression_disabled = 2
+both_disabled = 3
+
+buildOptions = both_disabled
+buildTypeString = ""
 
 parser = OptionParser(usage = "usage: %prog [options] mapfile");
 (options, args) = parser.parse_args()
@@ -21,18 +27,50 @@ print mapfile
 
 print '----------------------------------------------------------------------------'
 
+MEMORY_USAGE_FILE = '../..//memory_usage.txt'
+
 def check_release_build():
-    global isReleaseBuild
+    global buildOptions
+    global buildTypeString
     config_file_name = "../../../public/include/idigi_config.h"
 
+    isDebug = False
+    isCompression = False
     infile = open(config_file_name, "r")
     text = infile.read()
-    if text.find('IDIGI_NO_DEBUG') != -1:
-        isReleaseBuild = True
     infile.close()
+    if text.find('IDIGI_NO_DEBUG') == -1:
+        isDebug = True
+    if text.find('IDIGI_NO_COMPRESSION') == -1:
+        isCompression = True
+
+    # can use bit operation also
+    if isDebug and isCompression:
+        buildOptions = both_enabled
+    elif (not isDebug) and isCompression:
+        buildOptions = debug_disabled
+    elif isDebug and (not isCompression):
+        buildOptions = compression_disabled
+    else:
+        buildOptions = both_disabled
+
+    mem_usage_fd = open(MEMORY_USAGE_FILE, 'r')
+    mem_usage_text = mem_usage_fd.read()
+    mem_usage_fd.close()
+    if (mem_usage_text.find('All Enabled') == -1) and (buildOptions == both_enabled):
+        buildTypeString = 'All Enabled'
+    elif (mem_usage_text.find('Debug Disabled') == -1) and (buildOptions == debug_disabled):
+        buildTypeString = 'Debug Disabled'
+    elif (mem_usage_text.find('Compression Disabled') == -1) and (buildOptions == compression_disabled):
+        buildTypeString = 'Compression Disabled'
+    elif mem_usage_text.find('All Disabled') == -1:
+        buildTypeString = 'All Disabled'
+
+check_release_build()
+mem_usage_fd = open(MEMORY_USAGE_FILE, 'a')
 
 def verify_size_limit (actual_size, expected_size):
-    if isReleaseBuild:
+    if buildOptions == debug_disabled and buildOptions == both_disabled:
         if actual_size > expected_size:
             raise AssertionError
 
@@ -42,11 +80,15 @@ def print_kibibytes(text, value, type):
     large = str(value);
     decimal = large.index('.')
     print '%10s %8s KB %s' % (text, large[:decimal+4], type)
+    if buildTypeString != "":
+        mem_usage_fd.write('%10s %8s KB %s\n' % (text, large[:decimal+4], type))
 
 found_section_rodata_str1_1 = 0
 read_only_data_str1_1 = 0
    
-check_release_build()   
+if buildTypeString != "":
+    mem_usage_fd.write('-------- %s --------\n' %buildTypeString)
+
 for line in open(mapfile):
     
     elements = line.split()
@@ -101,4 +143,8 @@ for line in open(mapfile):
             print_kibibytes(section[1:], dec_size, '    uninitialized data zeroed by init code at startup')
             verify_size_limit(dec_size, bss_size_in_bytes)
 
+if buildTypeString != "":
+    mem_usage_fd.write('--------------------------------\n')
+
+mem_usage_fd.close()
 print '----------------------------------------------------------------------------'
