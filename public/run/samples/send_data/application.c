3629a0adc8ea2a9ad1d5f38b399a794ec8981154
Ming Liu-Chan <mlchan@digi.com>
2012-01-17 15:37:42 -0500

    Updated idigi_callback to app_idigi_callback

--- public/run/samples/send_data/application.c
+++ public/run/samples/send_data/application.c
@@ -38,7 +38,7 @@ extern idigi_callback_status_t app_data_service_handler(idigi_data_service_reque
 extern idigi_status_t app_send_put_request(idigi_handle_t handle);
 
 
-idigi_callback_status_t idigi_callback(idigi_class_t const class_id, idigi_request_t const request_id,
+idigi_callback_status_t app_idigi_callback(idigi_class_t const class_id, idigi_request_t const request_id,
                                     void * const request_data, size_t const request_length,
                                     void * response_data, size_t * const response_length)
 {
