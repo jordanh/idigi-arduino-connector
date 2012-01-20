59a99c927b6517185fb069e8750f9a29d4c1c441
Ming Liu-Chan <mlchan@digi.com>
2012-01-20 15:44:09 -0500

    Updated idigi_callback to app_idigi_callback

--- public/step/platforms/template/main.c
+++ public/step/platforms/template/main.c
@@ -28,7 +28,7 @@
 #include "idigi_api.h"
 #include "platform.h"
 
-extern idigi_callback_status_t idigi_callback(idigi_class_t const class_id, idigi_request_t const request_id,
+extern idigi_callback_status_t app_idigi_callback(idigi_class_t const class_id, idigi_request_t const request_id,
                                     void * const request_data, size_t const request_length,
                                     void * response_data, size_t * const response_length);
 
@@ -42,7 +42,7 @@ int main (void)
      * your callback function or add idigi_callback.
      *
      */
-    idigi_handle = idigi_init((idigi_callback_t) idigi_callback);
+    idigi_handle = idigi_init((idigi_callback_t) app_idigi_callback);
     if (idigi_handle == NULL)
     {
         APP_DEBUG("main: idigi_init() failed\n");
