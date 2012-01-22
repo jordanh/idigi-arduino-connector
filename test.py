bb9135da5ef4edd8ae2437fd8fc90bc9c797c6a2
Andrew Tolbert <x@andrewtolbert.com>
2012-01-22 14:31:34 -0600

    Made template compile unittests so result is captured.  Moved some functions to a utility file

--- test.py
+++ test.py
@@ -39,15 +39,12 @@ import idigi_ws_api
 import argparse
 import nose
 import iik_plugin
+import build_plugin
+from build_utils import get_template_dirs, setup_platform, build, sandbox
 sys.path.append('./dvt/scripts')
 import config
-from threading import Thread
 
 from stat import * # ST_SIZE etc
-
-TEMPLATE_TEST_DIR = 'dvt/samples/template_test'
-TEMPLATE_SCRIPT_DIR = 'dvt/cases/dvt_tests/'
-TEMPLATE_PLATFORM_DIR = 'public/run/platforms/template/'
 
 SAMPLE_DIR='public/run/samples/'
 SAMPLE_SCRIPT_DIR='dvt/cases/sample_tests/'
@@ -91,18 +88,6 @@ test_table = [
               [BASE_DVT_SRC+'data_service',         BASE_SCRIPT_DIR+'admin_tests',  ['test_nodebug_redirect.py']],
 ]
 
-def build_test(dir):
-
-    process = subprocess.Popen(['make', 'clean', 'all'], cwd=dir, 
-        stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    data = process.communicate()[0]
-    if process.returncode != 0:
-        print "+++FAIL: Build failed dir=[%s]" % dir
-        if data is not None:
-            print data
-    
-    return process.returncode
-
 def generate_id(api):
     """
     Generate a Pseudo Random Device Id (low probability of duplication) and 
@@ -126,20 +111,7 @@ def generate_id(api):
     # If here, we couldn't provision a device, raise Exception.
     raise Exception("Failed to Provision Device using %s." % user_id)
 
-def sandbox(directory):
-    """
-    Creates a temporary directory with contents of input directory.
-    This can be used to compile code and execute tests in a separate
-    place and will allow tests to run concurrently.
-    """
-    # Get a temporary directory name, then delete it.
-    sandbox_directory = tempfile.mkdtemp()
-    # Copy tree requires destination to not exist.
-    os.removedirs(sandbox_directory)
-    shutil.copytree(directory, sandbox_directory, symlinks=True)
-    return sandbox_directory
-
-def run_tests(description, base_dir, debug_on, api, replace_list=[], 
+def run_tests(description, base_dir, debug_on, api, cflags, replace_list=[], 
     update_config_header=False):
     sandbox_dir = sandbox(base_dir)
     device_location = None
@@ -153,7 +125,7 @@ def run_tests(description, base_dir, debug_on, api, replace_list=[],
                 os.path.join(sandbox_dir, SAMPLE_SCRIPT_DIR+'config.ini'))
 
         (device_id, mac_addr, device_location) = generate_id(api)
-        setup_platform(config, os.path.join(sandbox_dir, SAMPLE_SCRIPT_DIR), 
+        setup_platform(os.path.join(sandbox_dir, SAMPLE_SCRIPT_DIR), 
             os.path.join(sandbox_dir, SAMPLE_PLATFORM_DIR), mac_addr)
 
         for test in test_table:
@@ -163,7 +135,7 @@ def run_tests(description, base_dir, debug_on, api, replace_list=[],
      
             print '>>> [%s] Testing [%s]' % (description, src_dir)
 
-            rc = build_test(src_dir)
+            (rc,output) = build(src_dir, cflags)
 
             if rc != 0:
                 raise Exception("Failed to Build from %s." % src_dir)
@@ -253,10 +225,6 @@ def run_tests(description, base_dir, debug_on, api, replace_list=[],
             api.delete_location(device_location)
         shutil.rmtree(sandbox_dir)
 
-def setup_platform(config, config_dir, platform_dir, mac_addr):
-    config.remove_errors(platform_dir+'config.c')
-    config.update_config_source(platform_dir+'config.c', config_dir+'config.ini', mac_addr)
-
 def clean_output(directory):
     for root, folders, files in os.walk(directory):
         for test_result in filter(lambda f: f.endswith('.nxml') \
@@ -265,39 +233,25 @@ def clean_output(directory):
             print "Removing %s." % file_path
             os.remove(file_path)
 
-def get_template_dirs():
-    sandbox_dir = sandbox('.')
-    template_script_dir = os.path.join(sandbox_dir, TEMPLATE_SCRIPT_DIR)
-    template_platform_dir = os.path.join(sandbox_dir, TEMPLATE_PLATFORM_DIR)
-    template_test_dir = os.path.join(sandbox_dir, TEMPLATE_TEST_DIR)
-    idigi_config_path = os.path.join(sandbox_dir, 'public/include/idigi_config.h')
+def build_template(description, cflags):
 
-    return (sandbox_dir, template_script_dir, template_platform_dir, template_test_dir, idigi_config_path)
+    test_script = "test_build.py"
+    test_dir = BASE_SCRIPT_DIR+'template_tests'
 
-def build_template():
-    (sandbox_dir, template_script_dir, template_platform_dir, template_test_dir, idigi_config_path) = get_template_dirs()
-    setup_platform(config, template_script_dir, template_platform_dir, '000000:000000')
-    build_test(template_test_dir)
-    shutil.rmtree(sandbox_dir)
-
-    (sandbox_dir, template_script_dir, template_platform_dir, template_test_dir, idigi_config_path) = get_template_dirs()
-    setup_platform(config, template_script_dir, template_platform_dir, '000000:000000')
-    config.replace_string(idigi_config_path, 'IDIGI_COMPRESSION', 'IDIGI_NO_COMPRESSION')
-    config.replace_string(idigi_config_path, 'IDIGI_FIRMWARE_SERVICE', 'IDIGI_NO_FIRMWARE_SERVICE')
-    build_test(TEMPLATE_TEST_DIR)
-    shutil.rmtree(sandbox_dir)
-
-    (sandbox_dir, template_script_dir, template_platform_dir, template_test_dir, idigi_config_path) = get_template_dirs()
-    setup_platform(config, template_script_dir, template_platform_dir, '000000:000000')
-    config.replace_string(idigi_config_path, 'IDIGI_DATA_SERVICE', 'IDIGI_NO_DATA_SERVICE')
-    build_test(TEMPLATE_TEST_DIR)
-    shutil.rmtree(sandbox_dir)
-
-    (sandbox_dir, template_script_dir, template_platform_dir, template_test_dir, idigi_config_path) = get_template_dirs()
-    setup_platform(config, template_script_dir, template_platform_dir, '000000:000000')
-    config.replace_string(idigi_config_path, 'IDIGI_NO_FIRMWARE_SERVICE', 'IDIGI_FIRMWARE_SERVICE')
-    build_test(TEMPLATE_TEST_DIR)
-    shutil.rmtree(sandbox_dir)
+    print '>>> [%s] Executing [%s]' % (description, test_script)
+    
+    # Argument list to call nose with.  Generate a nosetest xml file in 
+    # current directory, pass in idigi / iik connection settings.
+    arguments = ['nosetests',
+                 '--with-xunit', 
+                 '--xunit-file=%s_%s.nxml' % (description, test_script),
+                 '--with-build',
+                 '--build_cflags=%s' % (cflags)]
+    
+    test_to_run = os.path.join(test_dir, test_script)
+    nose.run(defaultTest=[test_to_run], argv=arguments,
+        addplugins=[build_plugin.BuildPlugin()])
+    print '>>> [%s] Finished [%s]' % (description, test_script)
 
 def main():
     parser = argparse.ArgumentParser(description="IIK TestCase",
@@ -307,12 +261,19 @@ def main():
     parser.add_argument('--password', action='store', type=str, default='iik1sfun')
     parser.add_argument('--hostname', action='store', type=str, default='test.idigi.com')
     parser.add_argument('--descriptor', action='store', type=str, default='linux-x64')
+    parser.add_argument('--architecture', action='store', type=str, default='x64')
     parser.add_argument('--configuration', action='store', type=str, 
         default='all', choices=['default', 'nodebug', 'compression', 
                                     'debug', 'config_header', 'template', 
                                     'all'])
 
     args = parser.parse_args()
+
+    # If 64-bit not passed in, assume 32-bit
+    if args.architecture == 'x64':
+        cflags='DFLAGS=-m64'
+    else:
+        cflags='DFLAGS=-m32'
 
     api = idigi_ws_api.Api(args.username, args.password, args.hostname)
 
@@ -325,32 +286,32 @@ def main():
 
     if args.configuration == 'default' or args.configuration == 'all':
         print "============ Default ============="
-        run_tests('%s_%s' % (args.descriptor, 'Default'), '.', True, api)
+        run_tests('%s_%s' % (args.descriptor, 'Default'), '.', True, api, cflags)
 
     if args.configuration == 'nodebug' or args.configuration == 'all':
         print "============ No Debug ============="
-        run_tests('%s_%s' % (args.descriptor, 'Release'), '.', False, api, 
+        run_tests('%s_%s' % (args.descriptor, 'Release'), '.', False, api, cflags, 
         [('public/include/idigi_config.h', 'IDIGI_DEBUG', 'IDIGI_NO_DEBUG')])
 
     if args.configuration == 'compression' or args.configuration == 'all':
         print "============ Compression On ============="
-        run_tests('%s_%s' % (args.descriptor, 'Compression'), '.', False, api, 
+        run_tests('%s_%s' % (args.descriptor, 'Compression'), '.', False, api, cflags,
         [('public/include/idigi_config.h', 'IDIGI_NO_COMPRESSION', 
          'IDIGI_COMPRESSION')])
 
     if args.configuration == 'debug' or args.configuration == 'all':
         print "============ Debug On ============="
-        run_tests('%s_%s' % (args.descriptor, 'Debug'), '.', True, api, 
+        run_tests('%s_%s' % (args.descriptor, 'Debug'), '.', True, api, cflags, 
         [('public/include/idigi_config.h', 'IDIGI_NO_DEBUG', 'IDIGI_DEBUG')])
 
     if args.configuration == 'config_header' or args.configuration == 'all':
         print "============ Configurations in idigi_config.h ============="
-        run_tests('%s_%s' % (args.descriptor, 'idigiconfig'), '.', True, api, 
+        run_tests('%s_%s' % (args.descriptor, 'idigiconfig'), '.', True, api, cflags,
             update_config_header=True)
 
     if args.configuration == 'template' or args.configuration == 'all':
         print "============ Template platform ============="
-        build_template()
+        build_template(args.descriptor, cflags)
 
 if __name__ == '__main__':
     main()
