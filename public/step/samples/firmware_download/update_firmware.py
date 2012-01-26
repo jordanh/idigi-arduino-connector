677325a02b4d8365ee71b078a3654c9cf0630184
Vidya Shankar <vshankar@digi.com>
2012-01-26 12:48:50 -0500

    Vantive 41867, changed all *.py to executable and copied missing *.py to public/step

--- public/step/samples/firmware_download/update_firmware.py
+++ public/step/samples/firmware_download/update_firmware.py
@@ -18,33 +18,46 @@
 # Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
 #
 # ***************************************************************************
-# disconnect.py
-# Send disconnect SCI operation to disconnect iDigi.
+# update_firmware.py
+# Send an image file to do firmware upgrade using update_firmware SCI operation.
+# It updates firmware target = 0 with image file name "image.a"
 # -------------------------------------------------
-# Usage: disconnect.py <username> <password> <device_id>
+# Usage: update_firmware.py <username> <password> <device_id>
 # -------------------------------------------------
+image_file = "image.a" # image filename
 
 import httplib
 import base64
 import sys
 
 def Usage():
-    print 'Usage: disconnect.py <username> <password> <device_id>\n'
+    print 'Usage: update_firmware.py <username> <password> <device_id>'
+    print '       It opens \"image.a\" file and sends firmware update to device on target 0\n'  
    
 def PostMessage(username, password, device_id):
     # create HTTP basic authentication string, this consists of
     # "username:password" base64 encoded
     auth = base64.encodestring("%s:%s"%(username,password))[:-1]
     
-    # build disconnect message sent to server
+    # open download file and encoded the data
+    try: 
+        fileHandle = open(image_file, 'r')
+    except IOError:
+        print '\nError: cannot open', image_file
+        Usage();
+        return -1
+        
+
+    # build firmware download message sent to server
     message = """<sci_request version="1.0">
-        <disconnect>
+        <update_firmware firmware_target="0" filename="%s">
             <targets>
                 <device id="%s"/>
             </targets>
-        </disconnect>
+            <data>%s</data>
+        </update_firmware>
     </sci_request>
-    """%(device_id)
+    """%(image_file, device_id, base64.encodestring("%s"%fileHandle.read())[:-1])
     
     # to what URL to send the request with a given HTTP method
     webservice = httplib.HTTP("developer.idigi.com",80)
@@ -70,7 +83,8 @@ def PostMessage(username, password, device_id):
         print response_body
         
     webservice.close()
-            
+
+
 def main(argv):
     #process arguments
     count = len(argv);
@@ -82,4 +96,5 @@ def main(argv):
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv[1:]))
+
 
