3a64bb5bc2b94fa9b296888e9a9c6ae80d9f855a
Andrew Tolbert <x@andrewtolbert.com>
2012-01-30 14:48:23 -0600

    Add more descriptive information about what is being built/tested to help identify failure

--- dvt/cases/build_tests/test_build.py
+++ dvt/cases/build_tests/test_build.py
@@ -6,6 +6,7 @@ import sys
 class BuildTestCase(unittest.TestCase):
 
     def test_compile(self):
+        print "Building [%s] with CFLAGS [%s]." % (self.src, self.cflags)
         (rc, output) = build(self.src, self.cflags)
         if rc != 0:
             self.fail("%s:\n%s" % ("Did not cleanly compile", output)) 
