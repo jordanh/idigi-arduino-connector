7e686ca0ab8b8055a4fe87fe9d185a009da1e5d4
Ming Liu-Chan <mlchan@digi.com>
2012-01-31 11:27:04 -0500

    Removed disconnect test. Disconnect test is in a separate file

--- dvt/cases/dvt_tests/test_delay_reboot.py
+++ dvt/cases/dvt_tests/test_delay_reboot.py
@@ -25,15 +25,6 @@ REBOOT_REQUEST = \
   </reboot>
 </sci_request>"""
 
-DISCONNECT_REQUEST = \
-"""<sci_request version="1.0">
-    <disconnect>
-        <targets>
-            <device id="%s"/>
-        </targets>
-    </disconnect>
-</sci_request>"""
-
 def send_device_request(self, target_name, target_content):
 
     """ Verifies that the device request returned match the expected 
@@ -162,37 +153,5 @@ class RebootTestCase(iik_testcase.TestCase):
         finally:
             monitor.stop() 
             
-    def test_disconnect(self):
-    
-        """ Sends disconnect request to given device and verifies that
-        the device disconnects and reconnects to an iDigi server.
-        """
-        
-        self.log.info("***** Beginning Disconnect Test *****")
-        monitor = DeviceConnectionMonitor(self.api, self.device_config.device_id)
-
-        try:
-            monitor.start()
-            self.log.info("Sending Connection Control Disconnect to %s." % self.device_config.device_id)
-            
-            # Create disconnect request
-            disconnect_request = DISCONNECT_REQUEST % (self.device_config.device_id)
-            
-            # Send SCI disconnect request
-            self.api.sci(disconnect_request)
-        
-            
-            self.log.info("Waiting for iDigi to disconnect device.")
-            monitor.wait_for_disconnect(30)
-            self.log.info("Device disconnected.")
-            
-            self.log.info("Waiting for Device to reconnect.")
-            monitor.wait_for_connect(30)
-            self.log.info("Device connected.") 
-            time.sleep(10)
-        finally:
-            monitor.stop()
-                   
-
 if __name__ == '__main__':
     unittest.main() 
