40847b653d675f0bf1e573fadfb4ea3e07d6b0f3
Vidya Shankar <vshankar@digi.com>
2012-05-31 11:10:49 -0400

    Added copyright for some Freescale files. Removed common files from Freescale EW Demo

--- kits/freescale/source/firmware.c
+++ kits/freescale/source/firmware.c
@@ -1,28 +1,14 @@
 /*
- *  Copyright (c) 1996-2012 Digi International Inc., All Rights Reserved
- *
- *  This software contains proprietary and confidential information of Digi
- *  International Inc.  By accepting transfer of this copy, Recipient agrees
- *  to retain this software in confidence, to prevent disclosure to others,
- *  and to make no use of this software other than that for which it was
- *  delivered.  This is an unpublished copyrighted work of Digi International
- *  Inc.  Except as permitted by federal law, 17 USC 117, copying is strictly
- *  prohibited.
- *
- *  Restricted Rights Legend
- *
- *  Use, duplication, or disclosure by the Government is subject to
- *  restrictions set forth in sub-paragraph (c)(1)(ii) of The Rights in
- *  Technical Data and Computer Software clause at DFARS 252.227-7031 or
- *  subparagraphs (c)(1) and (2) of the Commercial Computer Software -
- *  Restricted Rights at 48 CFR 52.227-19, as applicable.
- *
- *  Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
- *
- * =======================================================================
- *
- */
-
+* Copyright (c) 2011, 2012 Digi International Inc.,
+* All rights not expressly granted are reserved.
+*
+* This Source Code Form is subject to the terms of the Mozilla Public
+* License, v. 2.0. If a copy of the MPL was not distributed with this file,
+* You can obtain one at http://mozilla.org/MPL/2.0/.
+*
+* Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
+* =======================================================================
+*/
 #include "idigi_api.h"
 #include "platform.h"
 #include "main.h"
@@ -74,6 +60,7 @@ static idigi_callback_status_t app_firmware_download_request(idigi_fw_download_r
     }
 
     /* Create buffer to be used by Flash Task */
+    #define FLASH_DOWNLOAD_BUFFER_SIZE 2048
     app_os_malloc(FLASH_DOWNLOAD_BUFFER_SIZE, &download_buffer);
     if (download_buffer == NULL)
     {
