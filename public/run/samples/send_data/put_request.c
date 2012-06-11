5134ffd9cada4731dfc6646f0cafa04fe47fa941
Vidya Shankar <vshankar@digi.com>
2012-06-11 13:53:32 -0400

    Modified send_data sample to demonstrate how user context is used

--- public/run/samples/send_data/put_request.c
+++ public/run/samples/send_data/put_request.c
@@ -16,20 +16,38 @@
 #include "idigi_api.h"
 #include "platform.h"
 
+#define SEND_DATA_SIZE  64
+typedef struct
+{
+    char const * data_ptr;
+    size_t bytes;
+} client_data_t;
+
 idigi_status_t app_send_put_request(idigi_handle_t handle)
 {
-    idigi_status_t status = idigi_success;
-    static idigi_data_service_put_request_t header;
-    static char file_path[] = "test/test.txt";
-    static char file_type[] = "text/plain";
+    idigi_status_t status = idigi_no_resource;
+    static idigi_data_service_put_request_t header; /* idigi connector will hold this until we get a response/error callback */
+    static char const file_path[] = "test/test.txt";
+    static char const file_type[] = "text/plain";
+    static char const buffer[] = "iDigi data service sample\n";
+    client_data_t * const app_data = malloc(sizeof *app_data);
 
+    if (app_data == NULL)
+        goto error;
+
+    app_data->data_ptr = buffer;
+    app_data->bytes = strlen(buffer);
     header.flags = IDIGI_DATA_PUT_APPEND;
     header.path  = file_path;
     header.content_type = file_type;
+    header.context = app_data; /* will be returned in all subsequent callbacks */
 
     status = idigi_initiate_action(handle, idigi_initiate_data_service, &header, NULL);
     APP_DEBUG("Status: %d, file: %s\n", status, file_path);
+    if (status != idigi_success)
+        free(app_data);
 
+error:
     return status;
 }
 
@@ -40,6 +58,8 @@ idigi_callback_status_t app_data_service_handler(idigi_data_service_request_t co
     idigi_callback_status_t status = idigi_callback_continue;
     idigi_data_service_msg_request_t const * const put_request = request_data;
     idigi_data_service_msg_response_t * const put_response = response_data;
+    idigi_data_service_put_request_t const * const header = put_request->service_context;
+    client_data_t * const app_data = (client_data_t * const)header->context;
 
     UNUSED_ARGUMENT(request_length);
     UNUSED_ARGUMENT(response_length);
@@ -50,6 +70,7 @@ idigi_callback_status_t app_data_service_handler(idigi_data_service_request_t co
          goto done;
     }
 
+    ASSERT(app_data != NULL);
     if (request == idigi_data_service_put_request)
     {
         switch (put_request->message_type)
@@ -57,14 +78,15 @@ idigi_callback_status_t app_data_service_handler(idigi_data_service_request_t co
         case idigi_data_service_type_need_data:
             {
                 idigi_data_service_block_t * const message = put_response->client_data;
-                char const buffer[] = "iDigi data service sample\n";
-                size_t const bytes = strlen(buffer);
 
-                if (message->length_in_bytes > bytes)
-                    message->length_in_bytes = bytes;
+                if (message->length_in_bytes > app_data->bytes)
+                    message->length_in_bytes = app_data->bytes;
 
-                memcpy(message->data, buffer, message->length_in_bytes);
-                message->flags = IDIGI_MSG_LAST_DATA | IDIGI_MSG_FIRST_DATA;
+                memcpy(message->data, app_data->data_ptr, message->length_in_bytes);
+                app_data->data_ptr += message->length_in_bytes;
+                app_data->bytes -= message->length_in_bytes;
+                if (app_data->bytes == 0)
+                    message->flags = IDIGI_MSG_LAST_DATA;
                 put_response->message_status = idigi_msg_error_none;
             }
             break;
@@ -72,15 +94,17 @@ idigi_callback_status_t app_data_service_handler(idigi_data_service_request_t co
         case idigi_data_service_type_have_data:
             {
                 idigi_data_service_block_t * const message = put_request->server_data;
-    
+
                 APP_DEBUG("Received %s response from server\n", ((message->flags & IDIGI_MSG_RESP_SUCCESS) != 0) ? "success" : "error");
                 if (message->length_in_bytes > 0) 
                 {
                     char * const data = message->data;
-    
+
                     data[message->length_in_bytes] = '\0';
                     APP_DEBUG("Server response %s\n", data);
                 }
+
+                free(app_data);
             }
             break;
 
@@ -90,10 +114,11 @@ idigi_callback_status_t app_data_service_handler(idigi_data_service_request_t co
                 idigi_msg_error_t const * const error_value = message->data;
 
                 APP_DEBUG("Data service error: %d\n", *error_value);
+                free(app_data);
             }
             break;
 
-        default:            
+        default:
             APP_DEBUG("Unexpected command: %d\n", request);
             break;
         }
