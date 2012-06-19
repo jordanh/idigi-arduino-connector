d3b87058132f4c1ca100cb46184aa71b1d73bdda
Andrew Tolbert <atolber@digi.com>
2012-06-19 13:37:25 -0500

    Build Config Tool and copy to idigi/tools in hudson.sh

--- hudson.sh
+++ hudson.sh
@@ -5,6 +5,7 @@ BASE_DIR=$WORKSPACE/idigi
 OUTPUT_DIR=$WORKSPACE/output
 PART_NUMBER=40003007
 PKG_NAME=${PART_NUMBER}_${REVISION}
+TOOLS_DIR=${BASE_DIR}/tools
 
 function cleanup () 
 {
@@ -24,8 +25,16 @@ function cleanup ()
     rm -r "${BASE_DIR}"
 }
 
+function build_config_tool ()
+{
+    mkdir -p "${TOOLS_DIR}"  
+    ant -f "${WORKSPACE}/tools/config/build.xml" -Ddist.dir="${TOOLS_DIR}"
+}
+
 # Create the output directory.
 mkdir -p "${OUTPUT_DIR}"
+
+build_config_tool
 
 # Create the doxygen documentation
 cd doxygen
