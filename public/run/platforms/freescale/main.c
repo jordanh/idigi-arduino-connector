bac8cc9b2eeb865df69c7530a8c857fa95f34410
Phil Krawitz <pkrawitz@netos-ubuntu-1.(none)>
2012-06-21 10:26:17 -0500

    Added missing Copyright statement

--- public/run/platforms/freescale/main.c
+++ public/run/platforms/freescale/main.c
@@ -1,8 +1,14 @@
-/****************************************************************************
-* 
-*   This file contains MQX only stationery code.
-*
-****************************************************************************/
+/*
+ * Copyright (c) 2012 Digi International Inc.,
+ * All rights not expressly granted are reserved.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * Digi International Inc. 11001 Bren Road East, Minnetonka, MN 55343
+ * =======================================================================
+ */
 #include "main.h"
 #include "platform.h"
 #include "os_support.h"
