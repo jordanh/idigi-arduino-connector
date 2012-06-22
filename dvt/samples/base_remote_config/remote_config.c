bfd8d3ac1c37d2b7a7ed5411cb4b5d39ddb17b98
Andrew Tolbert <atolber@digi.com>
2012-06-21 21:29:09 -0500

    [SIGNED-OFF] Handle indices and differentiate data set between setting and state

--- dvt/samples/base_remote_config/remote_config.c
+++ dvt/samples/base_remote_config/remote_config.c
@@ -31,76 +31,129 @@ void * ptr = 0;
 #define arrplen(a) sizeof(a)/sizeof(setting_data_t)
 
 typedef struct {
-  void ** group_data;
-  int capacity;
+    void ** group_data;
+    unsigned int group_id;
+    unsigned int index;
+    unsigned int capacity;
+    unsigned int count;
 } setting_data_t;
 
-setting_data_t ** setting_data = NULL;
-int setting_count = 0;
+typedef struct {
+    setting_data_t ** setting_data;
+    unsigned int capacity;
+    unsigned int count;
+} rci_data_t;
 
+rci_data_t rci_data[] = { 
+    { // setting data
+        NULL,
+        0,
+        0 
+    },
+    { // state data
+        NULL,
+        0,
+        0
+    }
+};
 
 typedef struct {
     void * group_context;
 } remote_group_session_t;
 
-void * get_setting_data(int group_id, int element_id, const void * def, int length);
-void add_setting_data(int group_id, int element_id, const void * data, int length);
+void * get_setting_data(idigi_remote_group_request_t const * const request, 
+    const void * def, int length);
+void add_setting_data(idigi_remote_group_request_t const * const request, 
+    const void * data, int length);
 
-void * get_setting_data(int group_id, int element_id, const void * def, int length){
-    // Init setting data if not set.
-    //APP_DEBUG("Getting Setting Data for %d:%d.\n", group_id, element_id);
-    if(setting_data == NULL){
-        setting_data = (setting_data_t **)malloc(10*sizeof(setting_data_t));
-        setting_count = 10;
+void * get_setting_data(idigi_remote_group_request_t const * const request, 
+    const void * def, int length){
+
+    unsigned int group_id = request->group.id;
+    unsigned int index = request->group.index;
+    unsigned int element_id = request->element.id;
+
+    rci_data_t * r_data = &rci_data[request->group.type];
+
+    unsigned int i;
+    for(i = group_id; i < r_data->count; i++){
+        setting_data_t * s_data = r_data->setting_data[i];
+        if(s_data != NULL && 
+            s_data->group_id == group_id && 
+            s_data->index == index &&
+            element_id < s_data->count &&
+            s_data->group_data[element_id] != NULL){
+            return s_data->group_data[element_id];
+        }
     }
-
-    if(group_id >= setting_count || setting_data[group_id] == NULL 
-        || element_id >= setting_data[group_id]->capacity
-        || setting_data[group_id]->group_data[element_id] == NULL){
-        // Data doesn't exist., add it.
-        add_setting_data(group_id, element_id, def, length);
-        // call again as data will be set by now.
-        return get_setting_data(group_id, element_id, def, length);
-    }
-
-    return setting_data[group_id]->group_data[element_id];
+    
+    // Data doesn't exist, add it.
+    add_setting_data(request, def, length);
+    return get_setting_data(request, def, length);
 }
 
-void add_setting_data(int group_id, int element_id, const void * data, int length){
+void add_setting_data(idigi_remote_group_request_t const * const request, 
+    const void * data, int length){
+
+    unsigned int group_id = request->group.id;
+    unsigned int index = request->group.index;
+    unsigned int element_id = request->element.id;
+
+    rci_data_t * r_data = &rci_data[request->group.type];
+
     // Init setting data if not set.
     //APP_DEBUG("Setting Data for %d:%d.\n", group_id, element_id);
-    if(setting_data == NULL){
-        setting_data = (setting_data_t **)malloc(10*sizeof(setting_data_t));
-        setting_count = 10;
+    if(r_data->setting_data == NULL){
+        r_data->setting_data = (setting_data_t **)malloc(10*sizeof(setting_data_t));
+        r_data->capacity = 10;
     }
 
     // If group id extends size of setting_data grow it.
-    if(group_id >= setting_count){ // resize array
-        setting_count = group_id + 10;
-        int new_size = setting_count * sizeof(setting_data_t);
-        setting_data = (setting_data_t **)realloc(setting_data, new_size);
+    if(r_data->count == r_data->capacity){ // resize array
+        r_data->capacity = r_data->capacity + 10;
+        int new_size = r_data->count * sizeof(setting_data_t);
+        r_data->setting_data = (setting_data_t **)realloc(r_data->setting_data, new_size);
     }
 
     // Init group data if not set.
-
-    setting_data_t * g_data = setting_data[group_id];
-    if(g_data == NULL){
-        g_data =(setting_data_t *)malloc(sizeof(setting_data_t));
-        g_data->group_data = (void **)malloc(1*sizeof(ptr));
-        g_data->capacity = 1;        
+    setting_data_t * s_data = NULL;
+    unsigned int i;
+    for(i = group_id; i < r_data->count; i++){
+        s_data = r_data->setting_data[i];
+        if(s_data != NULL && 
+            s_data->group_id == group_id && 
+            s_data->index == index){
+            // Found setting data;
+            break;
+        }
+        else{
+            s_data = NULL;
+        }
     }
 
-    if(element_id == g_data->capacity){ // resize array
-        g_data->capacity += 1;
-        int new_size = g_data->capacity * sizeof(ptr);
-        g_data->group_data = (void **)realloc(g_data->group_data, new_size);
+    // Allocate Setting Data if not found.
+    if(s_data == NULL){
+        s_data =(setting_data_t *)malloc(sizeof(setting_data_t));
+        r_data->setting_data[i] = s_data;
+        r_data->count = r_data->count + 1; 
+        s_data->group_id = group_id;
+        s_data->index = index;
+        s_data->group_data = (void **)malloc(10*sizeof(ptr));
+        s_data->capacity = 10;
+        s_data->count = 0;
+    }
+
+    while(s_data->count == s_data->capacity || 
+        element_id >= s_data->capacity){ // resize array
+        s_data->capacity += 10;
+        unsigned int new_size = s_data->capacity * sizeof(ptr);
+        s_data->group_data = (void **)realloc(s_data->group_data, new_size);
     }
 
     void * data_ptr = malloc(length);
     memcpy(data_ptr, data, length);
-    g_data->group_data[element_id] = data_ptr;
-
-    setting_data[group_id] = g_data;
+    s_data->group_data[element_id] = data_ptr;
+    s_data->count += 1; 
 }
 
 static idigi_callback_status_t app_rci_group_process(
@@ -135,59 +188,52 @@ static idigi_callback_status_t app_rci_group_process(
 static idigi_callback_status_t app_rci_group_get(
     idigi_remote_group_request_t const * const request,
     idigi_remote_group_response_t * const response){
-
-    //APP_DEBUG("app_rci_group_get. Type (%d).\n", request->element.type);
-
+    
     void * data;
 
     switch(request->element.type){
         case idigi_element_type_string:
         case idigi_element_type_multiline_string:
         case idigi_element_type_password:
-            data = get_setting_data(request->group.id, 
-                request->element.id, 
-                init_string, sizeof(char)*strlen(init_string));
+            data = get_setting_data(request, init_string, 
+                sizeof(char)*strlen(init_string));
             response->element_data.element_value->string_value = data;
             break;
         case idigi_element_type_int32:
-            data = get_setting_data(request->group.id, request->element.id, 
-                &init_signed, sizeof(int32_t));
+            data = get_setting_data(request, &init_signed, sizeof(int32_t));
             response->element_data.element_value->signed_integer_value = *(int32_t *)data;
             break;
         case idigi_element_type_uint32:
         case idigi_element_type_hex32:
         case idigi_element_type_0xhex:
-            data = get_setting_data(request->group.id, request->element.id, 
-                &init_unsigned, sizeof(uint32_t));
+            data = get_setting_data(request, &init_unsigned, 
+                sizeof(uint32_t));
             response->element_data.element_value->unsigned_integer_value = *(uint32_t *)data; 
             break;
         case idigi_element_type_float:
-            data = get_setting_data(request->group.id, 
-                request->element.id, &init_float, sizeof(float));
+            data = get_setting_data(request, &init_float, sizeof(float));
             response->element_data.element_value->float_value = init_float;
             break;
         case idigi_element_type_enum:
         case idigi_element_type_on_off:
         case idigi_element_type_boolean:
-            data = get_setting_data(
-                    request->group.id, request->element.id, &init_unsigned, 
-                    sizeof(uint32_t));
+            data = get_setting_data(request, &init_unsigned, sizeof(uint32_t));
             response->element_data.element_value->unsigned_integer_value = *(uint32_t *)data;
             break;
         case idigi_element_type_ipv4:
-            data = get_setting_data(request->group.id, request->element.id, 
-                init_ipv4, sizeof(char)*strlen(init_ipv4));
+            data = get_setting_data(request, init_ipv4, 
+                sizeof(char)*strlen(init_ipv4));
             response->element_data.element_value->string_value = data;
             break;
         case idigi_element_type_fqdnv4:
         case idigi_element_type_fqdnv6:
-            data =  get_setting_data(request->group.id, request->element.id, 
-                init_fqdnv4, sizeof(char)*strlen(init_fqdnv4));
+            data =  get_setting_data(request, init_fqdnv4, 
+                sizeof(char)*strlen(init_fqdnv4));
             response->element_data.element_value->string_value = data;
             break;
         case idigi_element_type_datetime:
-            data = get_setting_data(request->group.id, request->element.id, 
-                init_datetime, sizeof(char)*strlen(init_datetime)+1);
+            data = get_setting_data(request, init_datetime, 
+                sizeof(char)*strlen(init_datetime)+1);
             response->element_data.element_value->string_value = data;
             break;
         default:
@@ -202,7 +248,6 @@ static idigi_callback_status_t app_rci_group_set(
     idigi_remote_group_response_t * const response){
 
     UNUSED_ARGUMENT(response);
-    APP_DEBUG("app_rci_group_set. Type(%d).\n", request->element.type);
 
 
     switch(request->element.type){
@@ -213,30 +258,29 @@ static idigi_callback_status_t app_rci_group_set(
         case idigi_element_type_fqdnv4:
         case idigi_element_type_fqdnv6:
         case idigi_element_type_datetime:
-            add_setting_data(request->group.id, request->element.id, 
-                request->element.value->string_value, 
+            add_setting_data(request, request->element.value->string_value, 
                 sizeof(char)*strlen(request->element.value->string_value));
             break;
         case idigi_element_type_int32:
-            add_setting_data(request->group.id, request->element.id, 
+            add_setting_data(request,
                 &request->element.value->signed_integer_value, 
                 sizeof(request->element.value->signed_integer_value));
         case idigi_element_type_uint32:
         case idigi_element_type_hex32:
         case idigi_element_type_0xhex:
-            add_setting_data(request->group.id, request->element.id, 
+            add_setting_data(request,
                 &request->element.value->unsigned_integer_value, 
                 sizeof(request->element.value->unsigned_integer_value));
             break;
         case idigi_element_type_float:
-            add_setting_data(request->group.id, request->element.id, 
+            add_setting_data(request, 
                 &request->element.value->float_value, 
                 sizeof(request->element.value->float_value));
             break;
         case idigi_element_type_enum:
         case idigi_element_type_on_off:
         case idigi_element_type_boolean:
-            add_setting_data(request->group.id, request->element.id, 
+            add_setting_data(request,
                 &request->element.value->boolean_value, 
                 sizeof(request->element.value->boolean_value));
             break;
