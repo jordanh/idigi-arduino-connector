731ad2ecd35b72a9184930a0d30b90844701642a
Andrew Tolbert <Andrew.Tolbert@digi.com>
2012-06-28 17:12:24 -0500

    makegen changes to support dvt tests.  Remove accidentally committed files.  Add Makefile and idigi to .gitignore if location matches

--- dvt/scripts/makegen.py
+++ dvt/scripts/makegen.py
@@ -21,6 +21,9 @@ from string import Template
 # Template file to use for basis of Makefile.
 TEMPLATE = os.path.join(os.path.dirname(__file__), "Makefile.template")
 
+# Samples that only link and don't require platform code.
+LINK_SAMPLES = ['compileandlinkformetrics', 'compile_and_link']
+
 # Default Substitution Dictionary.
 DEFAULT_SUBS = {
     # Target Platform to build for.
@@ -50,7 +53,13 @@ CFLAGS += -I$(PLATFORM_DIR)""",
     'SRCS'                    : "SRCS = $(APP_SRCS) $(PLATFORM_SRCS) $(PRIVATE_SRCS)",
     # LIBS to include, -pthread will be added if 'run' type.  
     # connect_on_ssl adds '-lssl', file_system adds APP_ENABLE_MD5 if.
-    'LIBS'                    : "LIBS = -lc -lz"
+    'LIBS'                    : "LIBS = -lc -lz",
+    # Where to find iDigi private code.
+    'IDIGI_DIR'               : "../../../../private",
+    # Where to find public header files.
+    'PUBLIC_HEADER_DIR'       : "../../../include",
+    # What to include in sample make process, if anything.
+    'POST_SAMPLE'             : ""
 }
 
 def generate_makefile(path, make_template):
@@ -63,6 +72,9 @@ def generate_makefile(path, make_template):
     :param make_template: a Template object for a Makefile.
     """
     full_path = os.path.abspath(path)
+
+    # List of *.c files in the directory.
+    app_src = [ f for f in os.listdir(full_path) if f.endswith('.c') ]
     
     # Get the Mode and name of the Sample, this will be used for determining
     # what libraries and platform files to include.
@@ -70,34 +82,47 @@ def generate_makefile(path, make_template):
 
     subs = DEFAULT_SUBS.copy()
 
+    dvt_test = path.find('dvt') != -1
+
+    if dvt_test:
+        subs['IDIGI_DIR'] = '../../../private'
+        subs['PUBLIC_HEADER_DIR'] = '../../../public/include'
+        subs['PLATFORM_DIR'] = """
+# Location of Platform Src Code.
+PLATFORM_DIR=../../../public/run/platforms/$(PLATFORM)"""
+
     # Treat compile and link as a special case, no platform used.
-    if sample == 'compile_and_link':
+    if sample in LINK_SAMPLES:
         subs['TARGET_PLATFORM'] = ''
         subs['PLATFORM_DIR'] = ''
         subs['PLATFORM_VPATH'] = ''
         subs['PLATFORM_HEADER_INCLUDE'] = ''
         subs['SRCS'] = 'SRCS = $(APP_SRCS) $(PRIVATE_SRCS)'
     else:
-        # Assume this is the base set of Platform files 
-        subs['PLATFORM_SRCS'] = 'PLATFORM_SRCS = $(PLATFORM_DIR)/os.c $(PLATFORM_DIR)/config.c $(PLATFORM_DIR)/debug.c $(PLATFORM_DIR)/main.c'
+        # Assume this is the base set of Platform files
+        # Only include files who are also not in local directory.
+        subs['PLATFORM_SRCS'] = 'PLATFORM_SRCS = '
+        for f in ['os.c', 'config.c', 'debug.c', 'main.c']:
+            if f not in app_src:
+                subs['PLATFORM_SRCS'] += '$(PLATFORM_DIR)/%s ' % f
 
     # Add all *.c files in the directory.
     subs['APP_SRCS'] = 'APP_SRCS = ' + ' '.join([ re.sub(r'\.c$', '.c', f) \
-        for f in os.listdir(full_path) if f.endswith('.c') ])
+        for f in app_src ])
 
     # Add -lpthread as a linked library if this is a run sample.
-    if mode == 'run' and sample != 'compile_and_link':
+    if (dvt_test or mode == 'run') and sample not in LINK_SAMPLES:
         subs['LIBS'] += ' -lpthread' 
 
-    if sample == 'connect_on_ssl':
+    if sample == 'connect_on_ssl' and 'network_ssl.c' not in app_src:
         # Add network_ssl.c to PLATFORM_SRCS and -lssl to LIBS.
         subs['PLATFORM_SRCS'] += ' $(PLATFORM_DIR)/network_ssl.c'
         subs['LIBS'] += ' -lssl'
-    elif sample != 'compile_and_link':
+    elif sample not in LINK_SAMPLES and 'network.c' not in app_src:
         #  Otherwise add network.c.
         subs['PLATFORM_SRCS'] += ' $(PLATFORM_DIR)/network.c'
 
-    if sample == 'file_system':
+    if sample == 'file_system' and 'file_system.c' not in app_src:
         # Add file_system.c to PLATFORM_SRCS.  -lcrypto if APP_ENABLE_MD5
         # passed.
         subs['PLATFORM_SRCS'] += " $(PLATFORM_DIR)/file_system.c"
@@ -106,6 +131,9 @@ def generate_makefile(path, make_template):
 ifeq ($(APP_ENABLE_MD5),true)
 LIBS += -lcrypto
 endif"""
+
+    if dvt_test:
+        subs['POST_SAMPLE'] = '$(AT)python ../../../dvt/scripts/iikmapsum.py $(SAMPLE).map | tee $(basename $@).stats'
 
     return make_template.substitute(**subs)
 
@@ -126,6 +154,7 @@ if __name__ == "__main__":
     if sys.argv[1] == 'all':
         run_dir = os.path.abspath(os.path.join('.', 'public/run/samples'))
         step_dir = os.path.abspath(os.path.join('.', 'public/step/samples'))
+        dvt_dir = os.path.abspath(os.path.join('.', 'dvt/samples'))
 
         for d in os.listdir(run_dir):
             run_d = os.path.join(run_dir, d)
@@ -135,6 +164,13 @@ if __name__ == "__main__":
                 directories.append(step_d)
             if os.path.isdir(run_d):
                 directories.append(run_d)
+
+        for d in os.listdir(dvt_dir):
+            sample_dir = os.path.join(dvt_dir, d)
+
+            if os.path.isdir(sample_dir):
+                directories.append(sample_dir)
+
     else:
         directories.append(os.path.abspath(sys.argv[1]))
 
