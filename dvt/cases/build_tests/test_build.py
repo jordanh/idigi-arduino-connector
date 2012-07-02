7c1d4b09dbfc438265b49c8b227c46defe996055
Andrew Tolbert <Andrew.Tolbert@digi.com>
2012-07-02 17:41:11 -0500

    Log build output even if successful.
    Signed-off-by: Andrew Tolbert <Andrew.Tolbert@digi.com>

--- dvt/cases/build_tests/test_build.py
+++ dvt/cases/build_tests/test_build.py
@@ -2,6 +2,18 @@ import unittest
 import shutil
 from build_utils import build, generate_rci_code
 import sys
+import logging
+
+log = logging.getLogger('build_plugin')
+log.setLevel(logging.INFO)
+
+if len(log.handlers) == 0:
+    handler = logging.StreamHandler()
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    log.addHandler(handler)
+
 
 class BuildTestCase(unittest.TestCase):
 
@@ -17,3 +29,5 @@ class BuildTestCase(unittest.TestCase):
         (rc, output) = build(self.src, self.cflags)
         if rc != 0:
             self.fail("%s:\n%s" % ("Did not cleanly compile", output)) 
+        else:
+            log.info(output)
