361b0a83606820461bad9f7e605ff2d129740ed8
Ryan Bezdicek <ryan.bezdicek@digi.com>
2012-07-02 19:14:54 -0500

    Added query setting/state and set setting/state tests.
    Some of the state tests aren't failing because it is read_only.
    Once we get one that is read_write they should fail.
    Also added in logic to verify error string in the test.

--- dvt/cases/sample_tests/test_rci.py
+++ dvt/cases/sample_tests/test_rci.py
@@ -85,19 +85,23 @@ def send_request(rci_request):
     doc = send_rci(request)
 
     error = parse_error(doc)
-
-    log.info('Error: %s.  Description: %s.  Hint: %s' % error)
+    if error:
+        log.info('Error: %s.  Description: %s.  Hint: %s' % error)
+        perr=error[1]
+    else:
+        perr=None
+    assert_equal(perr,rci_request.error, 'Expected: "%s" Recieved: "%s"' % (rci_request.error,perr))
 
 def test_bad_rci_versions():
     tests = [
-            TestCase('version="1.0"', 
-                        '', 
-                        'version_1.0'),
             TestCase('version="2.0"', 
-                        '', 
+                        'Invalid version', 
                         'version_2.0'),
+            TestCase('version="1.0"', 
+                        'Invalid version', 
+                        'version_1.0'),
             TestCase('',
-                        '',
+                        None,
                         'version_blank'),
             ]
 
@@ -111,22 +115,73 @@ def test_bad_rci_versions():
 def test_query_descriptor():
     tests = [
             TestCase('<query_descriptor/>',
-                        '',
+                        'Bad command',
                         'query_descriptor_only'),
             TestCase('<query_descriptor></query_descriptor>',
-                        '',
+                        'Bad command',
                         'query_descriptor_blank'),
             TestCase('<query_descriptor><query_setting/></query_descriptor>',
-                        'Bad Command',
+                        'Bad command',
                         'query_setting'),
             TestCase('<query_descriptor><query_state/></query_descriptor>',
-                        '',
+                        'Bad command',
                         'query_state'),
             ]
 
     for test in tests:
         request = RCIRequest(test.test,
                                 test.error,
-                                'query_descriptor_%s'%test.name,
-                                DEFAULT_RCI_ATTRIBUTES)
+                                'query_descriptor_%s'%test.name)
+        yield send_request, request
+
+def test_query_setting_and_state():
+    tests = [
+            TestCase('<query_setting><unkownGroup/></query_setting>','Bad group','query_setting_unknown_group'),
+            TestCase('<query_state><unkownGroup/></query_state>','Bad group','query_state_unknown_group'),
+            TestCase('<query_setting><serial/></query_setting>',None,'query_setting_no_index_group'),
+            TestCase('<query_state><gps_stats/></query_state>',None,'query_state_no_index_group'),
+            TestCase('<query_setting><serial index="10"></serial></query_setting>',None,'query_setting_invalid_index_group'),
+            TestCase('<query_state><gps_stats index="10"></gps_stats></query_state>',None,'query_state_invalid_index_group'),
+            TestCase('<query_setting><serial/><system/></query_setting>',None,'query_setting_multiple_groups'),
+            #TestCase('<query_state><debug_info/><gps_stats/></query_state>',None,'query_state_multiple_groups'),
+            TestCase('<query_setting><serial/></query_setting>',None,'query_setting_no_element_1'),
+            TestCase('<query_state><gps_stats/></query_state>',None,'query_state_no_element_1'),
+            TestCase('<query_setting><serial></serial></query_setting>',None,'query_setting_no_element_2'),
+            TestCase('<query_state><gps_stats></gps_stats></query_state>',None,'query_state_no_element_2'),
+            TestCase('<query_setting><serial><unknownElement/></serial></query_setting>','Bad element','query_setting_unknown_element'),
+            TestCase('<query_state><gps_stats><unknownElement/></gps_stats></query_state>','Bad element','query_state_unknown_element'),
+            ]
+
+
+    for test in tests:
+        request = RCIRequest(test.test,test.error,test.name)
+        yield send_request, request
+
+def test_set_setting_and_state():
+    tests = [
+            TestCase('<set_setting><unkownGroup/></set_setting>','Parser error','set_setting_unknown_group'),
+            TestCase('<set_state><unkownGroup/></set_state>','Parser error','set_state_unknown_group'),
+            TestCase('<set_setting><system><description>Testing No Index</description></system></set_setting>',None,'set_setting_no_index_group'),
+            TestCase('<set_state><gps_stats><latitude>45</latitude></gps_stats></set_state>',None,'set_state_no_index_group'),
+            TestCase('<set_setting><system index="10"><description>Testing Invalid Index</description></system></set_setting>',None,'set_setting_invalid_index_group'),
+            TestCase('<set_state><gps_stats index="10"><latitude>45</latitude></gps_stats></set_state>',None,'set_state_invalid_index_group'),
+            TestCase('<set_setting><system><description>Testing Multiple Groups</description><contact>Testing Contact</contact></system></set_setting>',None,'set_setting_multiple_groups'),
+            #TestCase('<set_state><debug_info/><gps_stats/></set_state>',None,'set_state_multiple_groups'),
+            TestCase('<set_setting><system/></set_setting>','Parser error','set_setting_no_element_1'),
+            TestCase('<set_state><gps_stats/></set_state>','Parser error','set_state_no_element_1'),
+            TestCase('<set_setting><system></system></set_setting>','Parser error','set_setting_no_element_2'),
+            TestCase('<set_state><gps_stats></gps_stats></set_state>','Parser error','set_state_no_element_2'),
+            TestCase('<set_setting><system><unknownElement>Testing Unkown Element</unknownElement></system></set_setting>','Parser error','set_setting_unknown_element'),
+            TestCase('<set_state><gps_stats><unknownElement/></gps_stats></set_state>','Parser error','set_state_unknown_element'),
+            TestCase('<set_setting><system><description>asdfghjklaasdfghjklaasdfghjklaasdfghjklaasdfghjklaasdfghjklaasdfghjkla</description></system></set_setting>','Bad value','set_setting_out_of_range'),
+            TestCase('<set_state><gps_stats><latitude>9999999999999999999999999999999999999999999999999999999999999999999999999999999</latitude></gps_stats></set_state>','Bad value','set_state_out_of_range'),
+            TestCase("""<set_setting><system><description>Testing No Index
+                Multiple lines
+                to be bad test.</description></system></set_setting>""",'Bad value','set_setting_bad_type'),
+            TestCase('<set_state><gps_stats><latitude>0xff</latitude></gps_stats></set_state>','Bad value','set_state_bad_type'),
+            ]
+
+
+    for test in tests:
+        request = RCIRequest(test.test,test.error,test.name)
         yield send_request, request
