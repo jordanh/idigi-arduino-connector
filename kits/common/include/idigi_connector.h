c702fe8b9d70870c354902f74b5e618a6c13f5c2
Phil Krawitz <pkrawitz@netos-ubuntu-1.(none)>
2012-07-11 08:21:51 -0500

    Added more Doxygen commands and updated descriptions and return values

--- kits/common/include/idigi_connector.h
+++ kits/common/include/idigi_connector.h
@@ -75,124 +75,114 @@ typedef enum
 #define IDIGI_FLAG_LAST_DATA	0x10
 
 /** 
- * Data structure to hold the application/cloud data along with the user context.
- *
- * Parameters:
- *   data_ptr -- pointer to application/cloud data
- *   length_in_bytes -- number of bytes filled/available to fill in data_ptr
- *   flags -- Indicates archive/append in idigi_send_data, otherwise it indicates the last chunk.
- *   app_context -- pointer to hold application specific context
- *   error -- encountered connector error if any, application has to check this value before 
- *            handling the data
+ * Data structure to hold the application/cloud data along with the user context.            
  */
 typedef struct idigi_connector_data_t
 {
-    void * data_ptr;
-    size_t length_in_bytes;
-    unsigned int flags;
-    void * app_context;
-    idigi_connector_error_t error;
+    void * data_ptr;               /**< pointer to application/cloud data */
+    size_t length_in_bytes;        /**< number of bytes filled/available to fill in data_ptr */
+    unsigned int flags;            /**< Indicates archive/append in idigi_send_data, otherwise it indicates the last chunk */
+    void * app_context;            /**< pointer to hold application specific context */
+    idigi_connector_error_t error; /**< encountered connector error if any, application has to check this value before handling the data */
 }idigi_connector_data_t;
 
 /**
- * Device request callback function.  It is the callbacks responsibility to determine if the 
- * device request target is valid. Application need to check the error value in the request_data 
- * before processing the request.
+ * @brief Device request callback function.
  *
- * Parameters:
- *    target -- NUL-terminated device request target name
- *    request_data -- pointer to the request info, which contain requested data, data length,
- *                    flag to indicate last request data, connector error if any,
- *                    and application context. The value updated in the application context
- *                    will be retained in subsequent callbacks for the same target.
+ * This function needs to determine if the target and the data associated with that target is valid.
  *
- * Return Value:
- *    idigi_app_success -- for success
- *    idigi_app_busy -- application is not ready to receive this request
- *    idigi_app_unknown_target -- target not supported
- *    idigi_app_resource_error -- failed to allocate required resource
+ * @param target       NUL-terminated device request target name
+ * @param request_data pointer to the request info, which contain requested data, data length,
+ *                     flag to indicate last request data, connector error if any,
+ *                     and application context. The value updated in the application context
+ *                     will be retained in subsequent callbacks for the same target.
+ *
+ * @retval idigi_app_success        for success
+ * @retval idigi_app_busy           application is not ready to receive this request
+ * @retval idigi_app_unknown_target target not supported
+ * @retval idigi_app_resource_error failed to allocate required resource
  *
  */
 typedef idigi_app_error_t (* idigi_device_request_callback_t)(char const * const target, idigi_connector_data_t * const request_data);
 
 /**
- * Device response callback function.  It is the callbacks responsibility to determine if the 
- * target is valid. Application need to check the error value in the response_data before 
- * returning the response. 
- *  
- * Parameters:
- *    target -- NUL-terminated target name
- *    response_data -- pointer to the response info, which contain pointer to a buffer where user
- *                     can write the response, maximum buffer length, flag to indicate last response
- *                     data, connector error if any and the application context provided in the
- *                     first request callback.
+ * @brief Device response callback function.
  *
- * Return Value:
- *    -1 -- indicates error
- *    0  -- busy if last flag is not set
- *    0> -- indicates number of bytes copied to the response buffer, not more than max_response_bytes.
- *          The default max_response_bytes will be around 1590 bytes.
+ * This function checks for the result of the device request callback function, and sets up the
+ * response_data based on that result.  The Application needs to check the error value in the
+ * response_data before returning the response. 
+ *
+ * @param target        NUL-terminated target name
+ * @param response_data pointer to the response info, which contain pointer to a buffer where user
+ *                      can write the response, maximum buffer length, flag to indicate last response
+ *                      data, connector error if any and the application context provided in the
+ *                      first request callback.
+ *
+ * @retval -1 indicates error
+ * @retval 0  busy if last flag is not set
+ * @retval 0> indicates number of bytes copied to the response buffer, not more than max_response_bytes.
+ *            The default max_response_bytes will be around 1590 bytes.
  *
  */
 typedef size_t (* idigi_device_response_callback_t)(char const * const target, idigi_connector_data_t * const response_data);
 
 /**
- * Registers device request and response callback functions to deliver the request
- * from the iDigi Device Cloud and to return the response to the iDigi Device Cloud.
+ * @brief Registers the device request callbacks.
  *
- * Parameters:
- *    request_callback -- called with request data from the iDigi Device Cloud
- *    response_callback -- called to get the response data to the iDigi Device Cloud
+ * This function Registers device request and response callback functions to deliver the
+ * request from the iDigi Device Cloud and to return the response to the iDigi Device Cloud.
  *
- * Return Value:
- *    idigi_connector_success -- success
- *    idigi_connector_invalid_parameter -- NULL callback function
- *    idigi_connector_already_registered -- callback has already been registered 
+ * @param request_callback called with request data from the iDigi Device Cloud
+ * @param response_callback called to get the response data to the iDigi Device Cloud
+ *
+ * @retval idigi_connector_success success
+ * @retval idigi_connector_invalid_parameter NULL callback function
+ * @retval idigi_connector_already_registered callback has already been registered 
  */
 idigi_connector_error_t idigi_register_device_request_callbacks(idigi_device_request_callback_t request_callback, idigi_device_response_callback_t response_callback);
 
 /**
- * This function will send data to the iDigi Device Cloud.  Note, this is a network blocking call.
+ * @brief Send data to the iDigi Device Cloud.
  *
- * Parameters:
- *      path -- NUL-terminated file path where user wants to store the data on the iDigi Device Cloud
- *      device_data -- Will contain pointer to application data, data length in bytes, flags to indicate
- *                     whether append or archive is needed and application context (will be returned in subsequent callbacks)
- *      content_type -- NUL-terminated content type (text/plain, text/xml, application/json, etc). Pass NULL to let the iDigi Device Cloud determine
- *                      the type based on the file extension. In that case unsupported extensions will be treated as a binary data.
+ * This function will attempt to send data to the iDigi Device Cloud.  Note, this is a network blocking call.
  *
- * Return Value:
- *      idigi_connector_success -- success
- *      idigi_connector_invalid_parameter -- Indicates bad parameters
- *      idigi_connector_cloud_error -- Indicates error response from the iDigi Device Cloud
+ * @param path NUL terminated file path where user wants to store the data on the iDigi Device Cloud
+ * @param device_data Will contain pointer to application data, data length in bytes, flags to indicate
+ *                    whether append or archive is needed and application context (will be returned in subsequent callbacks)
+ * @param content_type NUL-terminated content type (text/plain, text/xml, application/json, etc). Pass NULL to let the iDigi Device Cloud determine
+ *                     the type based on the file extension. In that case unsupported extensions will be treated as a binary data.
+ *
+ * @retval idigi_connector_success success
+ * @retval idigi_connector_invalid_parameter Indicates bad parameters
+ * @retval idigi_connector_cloud_error Indicates error response from the iDigi Device Cloud
  */
 idigi_connector_error_t idigi_send_data(char const * const path, idigi_connector_data_t * const device_data, char const * const content_type);
 
 /**
- * Callback routine data type, which is called when there is any asynchronous error or 
- * reset message from the cloud/connector. After returning this callback, the 
- * application can restart the iDigi connector calling idigi_connector_start() again.
+ * @brief Callback routine data type.
  *
- * Parameters:
- *   status -- iDigi connector error value or reset
- *   status_message -- if available, this string explains more about the error
+ * This function is called when there is any asynchronous error or reset message from
+ * the cloud/connector. After returning this callback, the application can restart the
+ * iDigi connector by calling idigi_connector_start().
  *
- * Return value:
- *   none
+ * @param status iDigi connector error value or reset
+ * @param status_message if available, this string explains more about the error
+ *
+ * @retval none
  */
 typedef void (* idigi_status_callback_t)(idigi_connector_error_t const status, char const * const status_message);
 
 /**
- * Routine to start the iDigi connector. Any error during init 
- * may invoke the provided callback routine.  
- * 
- * Parameters: 
- *   status_callback -- to provide asynchronous status from iDigi connector
- * 
- * Return value: 
- *   idigi_connector_success -- iDigi connector started successfully
- *   idigi_connector_init_error -- Failed to initialize iDigi connector
- *   idigi_connector_resource_error -- Failed to create iDigi connector thread
+ * @brief Start the iDigi connector.
+ *
+ * This function will to try to start the iDigi connector.  Any error during init may
+ * invoke the provided callback routine.  
+ *  
+ * @param status_callback to provide asynchronous status from iDigi connector
+ *  
+ * @retval idigi_connector_success                 iDigi connector started successfully
+ * @retval idigi_connector_failed_to_create_thread Failed to create iDigi connector thread
+ * @retval idigi_connector_event_error             Failed to create iDigi connector event
  *
  */
 idigi_connector_error_t idigi_connector_start(idigi_status_callback_t status_callback);
