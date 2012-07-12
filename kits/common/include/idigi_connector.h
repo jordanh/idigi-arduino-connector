785677c1840f8d4876e9a018065f8b7c193851de
root <root@philk-HP-Compaq-8000-Elite-CMT-PC.(none)>
2012-07-12 12:56:13 -0400

    Added more doxygen for WindRiver

--- kits/common/include/idigi_connector.h
+++ kits/common/include/idigi_connector.h
@@ -87,7 +87,11 @@ typedef struct idigi_connector_data_t
 }idigi_connector_data_t;
 
 /**
- * @brief Device request callback function.
+ * @defgroup device_request_callback User defined device request callback function.
+ * @{
+ * @b Include: idigi_connector.h
+ */
+/**
  *
  * This function needs to determine if the target and the data associated with that target is valid.
  *
@@ -102,15 +106,52 @@ typedef struct idigi_connector_data_t
  * @retval idigi_app_unknown_target target not supported
  * @retval idigi_app_resource_error failed to allocate required resource
  *
+ * Example Usage:
+ * @code
+ *    #define BUFFER_SIZE 256
+ *    static char buffer[BUFFER_SIZE];
+ *    idigi_app_error_t status=idigi_app_invalid_parameter;
+ *
+ *    if (request_data->error != idigi_connector_success)
+ *    {
+ *        APP_DEBUG("devcie_request_callback: error [%d]\n", request_data->error);
+ *        goto error;
+ *    }
+ *
+ *    if (request_data->length_in_bytes < sizeof buffer)
+ *    {
+ *        memcpy(buffer, request_data->data_ptr, request_data->length_in_bytes);
+ *        buffer[request_data->length_in_bytes] = 0;
+ *    }
+ *    else
+ *    {
+ *        APP_DEBUG("device_request_callback: received more data than expected [%d]\n", request_data->length_in_bytes);
+ *        goto error;
+ *    }
+ *
+ *    APP_DEBUG("device_request_callback: received [%s] on target [%s]\n", buffer, target);
+ *    status = idigi_app_success;
+ *
+ * error:
+ *    return status;
+ * @endcode
+ *
  */
 typedef idigi_app_error_t (* idigi_device_request_callback_t)(char const * const target, idigi_connector_data_t * const request_data);
+/**
+* @}
+*/
 
 /**
- * @brief Device response callback function.
- *
+ * @defgroup device_response_callback User defined device response callback function.
+ * @{
+ * @b Include: idigi_connector.h
+ */
+/**
+ * 
  * This function checks for the result of the device request callback function, and sets up the
  * response_data based on that result.  The Application needs to check the error value in the
- * response_data before returning the response. 
+ * response_data before returning the response.
  *
  * @param target        NUL-terminated target name
  * @param response_data pointer to the response info, which contain pointer to a buffer where user
@@ -123,11 +164,39 @@ typedef idigi_app_error_t (* idigi_device_request_callback_t)(char const * const
  * @retval 0> indicates number of bytes copied to the response buffer, not more than max_response_bytes.
  *            The default max_response_bytes will be around 1590 bytes.
  *
+ * Example Usage:
+ * @code
+ *    static char rsp_string[] = "iDigi Connector device response!\n";
+ *    size_t const len = sizeof rsp_string - 1;
+ *    size_t const bytes_to_copy = (len < response_data->length_in_bytes) ? len : response_data->length_in_bytes;
+ *
+ *    if (response_data->error != idigi_connector_success)
+ *    {
+ *        APP_DEBUG("device_response_callback: error [%d]\n", response_data->error);
+ *        goto error;
+ *    }
+ *
+ *    memcpy(response_data->data_ptr, rsp_string, bytes_to_copy);
+ *    response_data->flags = IDIGI_FLAG_LAST_DATA;
+ *
+ *    APP_DEBUG("device_response_callback: target [%s], data- %s\n", target, rsp_string);
+ *
+ * error:
+ *    return bytes_to_copy;
+ * @endcode
+ *
  */
 typedef size_t (* idigi_device_response_callback_t)(char const * const target, idigi_connector_data_t * const response_data);
+/**
+* @}
+*/
 
 /**
- * @brief Registers the device request callbacks.
+ * @defgroup idigi_register_device_request_callbacks Registers the device request callbacks.
+ * @{ 
+ * @b Include: idigi_connector.h
+ */
+/**
  *
  * This function Registers device request and response callback functions to deliver the
  * request from the iDigi Device Cloud and to return the response to the iDigi Device Cloud.
@@ -140,11 +209,18 @@ typedef size_t (* idigi_device_response_callback_t)(char const * const target, i
  * @retval idigi_connector_already_registered callback has already been registered 
  */
 idigi_connector_error_t idigi_register_device_request_callbacks(idigi_device_request_callback_t request_callback, idigi_device_response_callback_t response_callback);
+/**
+* @}
+*/
 
 /**
- * @brief Send data to the iDigi Device Cloud.
+ * @defgroup idigi_send_data Send data to a file on the iDigi Device Cloud.
+ * @{ 
+ * @b Include: idigi_connector.h
+ */
+/** 
  *
- * This function will attempt to send data to the iDigi Device Cloud.  Note, this is a network blocking call.
+ * This function will attempt to send data to a file on the iDigi Device Cloud.  Note, this is a network blocking call.
  *
  * @param path NUL terminated file path where user wants to store the data on the iDigi Device Cloud
  * @param device_data Will contain pointer to application data, data length in bytes, flags to indicate
@@ -155,25 +231,45 @@ idigi_connector_error_t idigi_register_device_request_callbacks(idigi_device_req
  * @retval idigi_connector_success success
  * @retval idigi_connector_invalid_parameter Indicates bad parameters
  * @retval idigi_connector_cloud_error Indicates error response from the iDigi Device Cloud
+ *
+ * Example Usage:
+ * @code
+ *    idigi_connector_error_t ret;
+ *    char content_type[] = "text/plain";
+ *    char path[] = 'test.txt";
+ *    char buffer[] = "This is a test";
+ *    unsigned int flags = 0; // 0 = overwrite file
+ *
+ *    do
+ *    {
+ *        static idigi_connector_data_t device_data = {0};
+ *
+ *        device_data_data.data_ptr = buffer;
+ *        device_data_data.length_in_bytes = strlen(buffer);
+ *        device_data_data.flags = flags;
+ *
+ *        ret = idigi_send_data(path, &device_data, content_type);
+ *        if (ret == idigi_connector_init_error)
+ *        {
+ *            #define WAIT_FOR_A_SECOND  1
+ *            app_os_sleep(WAIT_FOR_A_SECOND);
+ *        }
+ *
+ *    } while (ret == idigi_connector_init_error);
+ * @endcode
  */
 idigi_connector_error_t idigi_send_data(char const * const path, idigi_connector_data_t * const device_data, char const * const content_type);
+/**
+* @}
+*/
 
 /**
- * @brief Callback routine data type.
- *
- * This function is called when there is any asynchronous error or reset message from
- * the cloud/connector. After returning this callback, the application can restart the
- * iDigi connector by calling idigi_connector_start().
- *
- * @param status iDigi connector error value or reset
- * @param status_message if available, this string explains more about the error
- *
  * @defgroup idigi_status_callback_t Application-defined status_callback
  *@{ 
  * idigi_status_callback_t: iDigi connector Application-defined status_callback, this is the 
  * callback used when there is any asynchronous error or reset message from
  * the cloud/connector. After returning this callback, the application can restart the
- * iDigi connector by calling idigi_connector_start()..
+ * iDigi connector by calling idigi_connector_start().
  *
  */
 /** 
@@ -190,7 +286,11 @@ typedef void (* idigi_status_callback_t)(idigi_connector_error_t const status, c
 */
 
 /**
- * @brief Start the iDigi connector.
+ * @defgroup idigi_connector_start Start the iDigi connector.
+ * @{ 
+ * @b Include: idigi_connector.h
+ */
+/** 
  *
  * This function will to try to start the iDigi connector.  Any error during init may
  * invoke the provided callback routine.  
@@ -201,8 +301,18 @@ typedef void (* idigi_status_callback_t)(idigi_connector_error_t const status, c
  * @retval idigi_connector_failed_to_create_thread Failed to create iDigi connector thread
  * @retval idigi_connector_event_error             Failed to create iDigi connector event
  *
+ * Example Usage:
+ * @code
+ *    idigi_connector_error_t result = idigi_connector_start(status_callback);
+ * @endcode
+ *  
+ * @see idigi_status_callback_t
+ *
  */
 idigi_connector_error_t idigi_connector_start(idigi_status_callback_t status_callback);
+/**
+* @}
+*/
 
 /**
 * @}.
