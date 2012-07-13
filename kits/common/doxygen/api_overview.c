393e75911817875e4fa2fa1f3ffc7fdfea63e5e9
Philip Krawitz <pkrawitz@digi.com>
2012-07-13 10:23:33 -0400

    Added more doxygen

--- kits/common/doxygen/api_overview.c
+++ kits/common/doxygen/api_overview.c
@@ -53,22 +53,48 @@
  *
  * @subsection send_data Send Data: idigi_send_data() 
  * 
- * The function idigi_send_data() is used to write data to a file on the iDigi Server.
+ * The function idigi_send_data() is used to write data to a file on the iDigi Server specified by parameters path, file-name,
+ * content-type, data to be written to the file, length of the data and flags.  The flags are used to specify how the data
+ * will be written to the file.  The flag definitions are listed below:
  *
+ * @htmlonly
+ * <table class="apitable">
+ * <tr>
+ * <th class="title">Flag</th>
+ * <th class="title">Description</th>
+ * </tr>
+ * <tr>
+ * <td>@endhtmlonly @ref IDIGI_FLAG_OVERWRITE_DATA @htmlonly</td>
+ * <td>Tells the iDigi Device Cloud to overwrite the file with the new data</td>
+ * </tr>
+ * <tr>
+ * <td>@endhtmlonly @ref IDIGI_FLAG_ARCHIVE_DATA @htmlonly</td>
+ * <td>Tells the iDigi Device Cloud to keep a history of the changes to the file</td>
+ * </tr>
+ * <tr>
+ * <td>@endhtmlonly @ref IDIGI_FLAG_APPEND_DATA @htmlonly</td>
+ * <td>Tells the iDigi Device Cloud to append the new data to the existing file</td>
+ * </tr>
+ * </table>
+ * @endhtmlonly
+ *  
  * @subsection register_device_request_callbacks Register Device Request Callbacks: idigi_register_device_request_callbacks() 
  *
- * The function idigi_register_device_request_callbacks() must be called in order to receive device requests from the iDigi
- * Device Cloud.  You must register a device_request_callback and a device_response_callback.
+ * The function idigi_register_device_request_callbacks() must be called in order to receive and to respond to device requests
+ * from the iDigi Device Cloud.  You must register a device_request_callback and a device_response_callback.
  *
  * @subsection device_request_callback Device Request Callback: device_request_callback() 
  *
  * The user defined function @ref idigi_device_request_callback_t "device_request_callback()" parses the device request received
- * from the iDigi Device Cloud.
+ * from the iDigi Device Cloud to determine if the target and the data associated with that target is valid.  If both are valid,
+ * the user defined function @ref idigi_device_request_callback_t "device_request_callback()" can turn on/off an Led, set the
+ * state of a GPIO pin, or set the value of a variable.
  *
  * @subsection device_response_callback Device Response Callback: device_response_callback() 
  *
- * The user defined function @ref idigi_device_response_callback_t "device_response_callback()" Prepares the response to the device
- * request to be sent to the iDigi Device Cloud.
+ * The user defined function @ref idigi_device_response_callback_t "device_response_callback()" checks the result of the @ref
+ * idigi_device_request_callback_t "device_request_callback()", and sets up the response_data based on that result.  The
+ * Application needs to check the error value in the response_data before returning the response.
  *
  * @htmlinclude terminate.html
  *
